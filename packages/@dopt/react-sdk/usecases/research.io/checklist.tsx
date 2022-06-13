cffb90bc124b5ab49621b4ec61de75e11e886a65
Joe McKenney <joe@switchboard.cc>
2022-06-13 16:36:16 -0700

    docs(@dopt/react): iterate on use cases && add your use case (#528)
    * docs(@dopt/react): iterate on use cases && add your use case
    
    * refactor(@dopt/react): updates to usecases based on feedback
    
    * refactor(@dopt/react): update the use case / scaffold out the calendar page

---
+++ packages/@dopt/react-sdk/usecases/research.io/checklist.tsx
@@ -0,0 +1,44 @@
+import { ChecklistItem, ChecklistProgress } from '@research.io/checklist';
+import { useDopt, groupStats } from '@dopt/react';
+
+const Checklist = () => {
+  const [calendarIntegration, calendarIntegrationActions] = useDopt(
+    'calendar-integration-100dae3'
+  );
+  const [scheduleMeeting, scheduleMeetingActions] = useDopt(
+    'schedule-meeting-a40ea81'
+  );
+  const [interview, interviewActions] = useDopt('interview-038dd7b');
+
+  const checkListItems = [calendarIntegration, scheduleMeeting, interview];
+
+  if (!checkListItems.every(({ active }) => active)) {
+    return null;
+  }
+
+  const { percentageDone, numFinished, total } = groupStats(...checkListItems);
+
+  return (
+    <div>
+      <h3>👋 Welcome to Ethnio!</h3>
+      <p>Get started by completing these quick steps.</p>
+      <ChecklistProgress value={percentageDone} />
+      {numFinished} / {total}
+      <ChecklistItem
+        label="Set up calendar integration"
+        onClick={calendarIntegrationActions.start}
+        finished={calendarIntegration.finished}
+      />
+      <ChecklistItem
+        label="Schedule first interview"
+        onClick={scheduleMeetingActions.start}
+        finished={scheduleMeeting.finished}
+      />
+      <ChecklistItem
+        label="Complete interview"
+        onClick={interviewActions.start}
+        finished={interview.finished}
+      />
+    </div>
+  );
+};
