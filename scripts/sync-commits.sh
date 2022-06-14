3e5591f3f7d42494912e1a0b80f79fc21d27d0c4
Joe McKenney <joe@switchboard.cc>
2022-06-14 10:42:11 -0700

    chore(odopt): add script for syncing commits across repos

---
+++ scripts/sync-commits.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pushd /tmp;
+
+# source
+git clone git@github.com:dopt/dopt.git;
+
+# target
+git clone git@github.com:dopt/odopt.git;
+
+pushd dopt;
+
+git filter-repo \
+  --path tsconfig.base.json \
+  --path packages/@dopt/react-sdk \
+  --path packages/@dopt/config \
+  --path packages/@dopt/check-formatting;
+
+popd;
+
+pushd odopt;
+
+git remote add dopt  ../dopt;
+
+git pull dopt main --allow-unrelated-histories;
+
+git push origin main;
+
+popd;
+popd;
