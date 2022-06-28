706a39bb1e8eb9c30e4ef29ff7c8961d78e02767
Joe McKenney <joe@switchboard.cc>
2022-06-27 19:18:23 -0700

    refactor(dopt): all packages use tsup.config.ts (#622)
    * refactor(dopt): remaining tsup.config.ts files extend shared tsup config
    
    * refactor(dopt): refactor package script tsup config to use tsup.config.ts
    
    * feat(dopt): all packages use tsup config file
    
    * feat(dopt): all packages use tsup config file
    
    * feat(dopt): update tsconfig for schema
    
    * feat(dopt): remove accidentally checked in type definition & maps
    
    * feat(dopt): move @dopt/tsconfig package into config package
    
    * feat(dopt): remove tsconfig package
    
    * feat(dopt): remove dep on tsconfig
    
    * feat(dopt): move tsconfig into config
    
    * feat(dopt): get the build passing again
    
    * fix(dopt): ensure packages with non-standard src/ build types correctly
    
    * refactor(@bodo/svg): rearrange src/ and artifacts to more easily build
    
    * fix(server.app.apps): clean up typechecking

--- packages/@dopt/react-sdk/tsup.config.ts
+++ packages/@dopt/react-sdk/tsup.config.ts
@@ -1,17 +1,2 @@
-import { defineConfig } from 'tsup';
-import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
-import { peerDependencies } from './package.json';
-
-export default defineConfig((options) => ({
-  entry: ['src/index.ts'],
-  outDir: 'dist',
-  format: ['esm', 'cjs'],
-  inject: ['./.react-jsx-inject.ts'],
-  sourcemap: true,
-  bundle: true,
-  minify: !options.watch,
-  dts: true,
-  esbuildPlugins: [vanillaExtractPlugin()],
-  external: [...Object.keys(peerDependencies)],
-  clean: !options.watch,
-}));
+import { tsup } from '@dopt/config';
+export default tsup.react({ entry: ['src/index.ts'] });
