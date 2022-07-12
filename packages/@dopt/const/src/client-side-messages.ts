efd7438a63cde390ef2f0d296798661ac45420ce
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-07-12 10:31:23 -0700

    feat(@app/client): display alert on block update/delete error (#737)
    * feat(@app/client): display alert on block update/delete error
    
    * fix(@app/client): remove try/catch from journey hook
    
    * refactor(@app/client): remove rename refactor code
    
    * refactor(@app/client): moving alert to modal

---
+++ packages/@dopt/const/src/client-side-messages.ts
@@ -0,0 +1,2 @@
+export const SAVE_FAIL_MSG = 'Could not save block. Please try again.';
+export const DELETE_FAIL_MSG = 'Could not delete block. Please try again.';
--- packages/@dopt/const/src/index.ts
+++ packages/@dopt/const/src/index.ts
@@ -4,3 +4,4 @@ export * from './health-check';
 export * from './operations';
 export * from './blocks';
 export * from './journey-moves';
+export * from './client-side-messages';
