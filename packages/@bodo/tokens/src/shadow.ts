82d58a713144ebddc49cc46fbeb39dc76afa617e
Alex Chan <alexchantastic@users.noreply.github.com>
2022-07-15 11:54:18 -0700

    feat(@bodo/tokens): update shadow tokens (#787)

--- packages/@bodo/tokens/src/shadow.ts
+++ packages/@bodo/tokens/src/shadow.ts
@@ -1,13 +1,13 @@
 import chroma from 'chroma-js';
 import { colors } from './color';
 
-const shadowColor = chroma(colors.base.black).alpha(0.2).css();
+const baseShadowColor = chroma(colors.base.black);
 
 export const shadows = {
   0: 'none',
-  1: `0 0 2px ${shadowColor}`,
-  2: `0 1px 2px ${shadowColor}`,
-  3: `0 2px 4px ${shadowColor}`,
-  4: `0 4px 4px ${shadowColor}`,
-  5: `0 8px 12px ${shadowColor}`,
+  1: `0 0 4px ${baseShadowColor.alpha(0.04).css()}`,
+  2: `0 2px 4px ${baseShadowColor.alpha(0.04).css()}`,
+  3: `0 4px 8px ${baseShadowColor.alpha(0.04).css()}`,
+  4: `0 4px 12px 4px ${baseShadowColor.alpha(0.08).css()}`,
+  5: `0 8px 20px 8px ${baseShadowColor.alpha(0.08).css()}`,
 };
