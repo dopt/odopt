3e9a1e7b97feefa85c6759965a3c955f41b707ce
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-07-21 15:13:32 -0700

    feat(@app/client): reset journey state button (#850)
    * feat(@app/client): reset journey state button
    
    Adds reset journey state button to user details page. Adds internal client route to block service to
    reset journey state.
    
    * refactor(@app/client): styling
    
    * fix(@app/client): misc fixes
    
    Closing confirmation prompt on success. Fixing app/server journeys endpoint to get journeys by
    workspaceId.
    
    * refactor(@bodo/confirmation-prompt): import button styles

--- packages/@dopt/const/src/client-side-messages.ts
+++ packages/@dopt/const/src/client-side-messages.ts
@@ -1,2 +1,3 @@
 export const SAVE_FAIL_MSG = 'Could not save block. Please try again.';
 export const DELETE_FAIL_MSG = 'Could not delete block. Please try again.';
+export const RESET_FAIL_MSG = 'Could not reset journey. Please try again.';
