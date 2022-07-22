7c8e5c13a0e102406ebcb1b2f5acf8aa9f5e996c
Joe McKenney <joe@switchboard.cc>
2022-07-22 15:27:20 -0700

    docs(@dopt/react): a first pass at typedoc for the react sdk (#869)
    * docs(@dopt/react): tsdoc for the react sdk
    
    * docs(@dopt/react): finish first pass on docs before publish
    
    * docs(@dopt/react): another pass as typedoc + examples
    
    * fix(@dopt/react): update example

--- packages/@dopt/react-sdk/src/context.ts
+++ packages/@dopt/react-sdk/src/context.ts
@@ -1,6 +1,10 @@
 import { createContext } from 'react';
 import { Blocks, Methods } from './types';
 
+/**
+ * The context accessible through either the {@link useDopt}Hook
+ * or the {@link withDopt}HOC
+ */
 type DoptContext = {
   blocks: Blocks;
   methods: Methods;
