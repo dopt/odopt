cd65573dec631a7588b157026e8b9b9e6c59248c
Joe McKenney <joe@switchboard.cc>
2022-07-22 17:03:18 -0700

    fix(@dopt/react): docs updates and export relevant types (#876)

--- packages/@dopt/react-sdk/src/index.ts
+++ packages/@dopt/react-sdk/src/index.ts
@@ -2,3 +2,13 @@ export { BaseDoptProvider as DoptProvider } from './base-provider';
 export { DoptContext } from './context';
 export { useDopt } from './use-dopt';
 export { withDopt } from './with-dopt';
+
+export type { Methods } from './use-dopt';
+export type {
+  Block,
+  Blocks,
+  Mocks,
+  BaseProviderConfig,
+  ProviderConfig,
+  MockProviderConfig,
+} from './types';
