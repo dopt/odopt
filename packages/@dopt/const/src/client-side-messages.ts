cde1331a0cce30e79abed255114341bbef7016c8
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2022-07-25 15:17:22 -0700

    feat(@app/client): add missing confirmation prompt for regenerate APIKey (#882)
    * feat(@app/client): add missing confirmation prompt for regenerate APIKey
    
    * fix(@app/client): spelling mistake
    
    * fix(@app/client): addressing review comments
    
    * fix(@app/client): addressing review comments

--- packages/@dopt/const/src/client-side-messages.ts
+++ packages/@dopt/const/src/client-side-messages.ts
@@ -1,3 +1,5 @@
 export const SAVE_FAIL_MSG = 'Could not save block. Please try again.';
 export const DELETE_FAIL_MSG = 'Could not delete block. Please try again.';
 export const RESET_FAIL_MSG = 'Could not reset journey. Please try again.';
+export const REGENERATE_FAIL_MSG =
+  'Could not regenerate API key. Please try again.';
