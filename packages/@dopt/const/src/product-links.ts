826c7f0b6cfde3ddb4d282dea508038257168113
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2022-07-27 11:17:14 -0700

    fix(@app/client): update react-sdk docs link (#909)

--- packages/@dopt/const/src/product-links.ts
+++ packages/@dopt/const/src/product-links.ts
@@ -1,2 +1,4 @@
 export const DOCS_LINK =
   'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930';
+
+export const REACT_SDK_LINK = 'https://react-sdk-docs.vercel.app/index.html';
