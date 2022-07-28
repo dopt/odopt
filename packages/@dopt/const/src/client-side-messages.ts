42bb24b40ee5673ca43288810212ddb2a852a605
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-07-28 16:32:32 -0700

    feat(@app/client): show toast for failed journey creation (#916)
    * feat(@app/client): show toast for failed journey creation
    
    * refactor(@app/client): remove error
    
    * refactor(@app/client): remove unused imports

--- packages/@dopt/const/src/client-side-messages.ts
+++ packages/@dopt/const/src/client-side-messages.ts
@@ -3,3 +3,6 @@ export const DELETE_FAIL_MSG = 'Could not delete block. Please try again.';
 export const RESET_FAIL_MSG = 'Could not reset journey. Please try again.';
 export const REGENERATE_FAIL_MSG =
   'Could not regenerate API key. Please try again.';
+
+export const JOURNEY_CTA_FAIL_MSG = 'Could not create journey';
+export const TRY_AGAIN_MSG = 'Please try again';
