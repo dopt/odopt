ac969434cab1b6fe8ad095b800b57158d9ab3ee6
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-07-29 08:38:46 -0700

    fix(@app/client): empty state links should redirect to docs (#927)
    * fix(@app/client): empty state links should redirect to docs
    
    * fix(@app/client): fixing link usage

--- packages/@dopt/const/src/product-links.ts
+++ packages/@dopt/const/src/product-links.ts
@@ -2,3 +2,15 @@ export const DOCS_LINK =
   'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930';
 
 export const REACT_SDK_LINK = 'https://react-sdk-docs.vercel.app/index.html';
+
+export const LEARN_MORE_JOURNEYS_LINK =
+  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#49a58cfb0c744256a8000ad02fe9ca96';
+
+export const LEARN_MORE_USERS_LINK =
+  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#4c15c8cf816f4ea2aff5f18b29350634';
+
+export const LEARN_MORE_APIKEY_LINK =
+  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#b717a53914744182ab0a4eacd809d236';
+
+export const LEARN_MORE_USER_LINK =
+  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#4c15c8cf816f4ea2aff5f18b29350634';
