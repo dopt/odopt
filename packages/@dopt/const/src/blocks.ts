5e371ad2e4ae8add8c1d965102de9d1b82349203
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-08-01 14:59:04 -0700

    fix(@app/client): long text overflows (#949)
    * fix(@app/client): long text overflows
    
    * refactor(@app/client): misc refactor
    
    remove wordBreak attribute from CSS, update truncate prop
    
    * fix(@app/client): console error for <colgroup>

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -10,7 +10,7 @@ export type BlockType =
 
 export const CHARACTER_LIMIT_NAME = 100;
 
-export const BLOCK_NAME_LENGTH_MSG = `Block name can't be more than ${CHARACTER_LIMIT_NAME} characters`;
+export const BLOCK_NAME_LENGTH_MSG = `Name can't be more than ${CHARACTER_LIMIT_NAME} characters`;
 
 export const ENTRY_BLOCK_DEFAULT_DESC = 'How users enter into the journey';
 
