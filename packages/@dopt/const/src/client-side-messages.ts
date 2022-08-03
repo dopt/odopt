927ca571d8e5e990949c574e1b855968fec734c1
mariaorias <102619044+mariaorias@users.noreply.github.com>
2022-08-03 09:15:31 -0700

    feat(@app/client): error handling for settings page (#970)
    * feat(@app/client): error handling for settings page
    
    * fix(@app/client): clearing error state when api call succeeds

--- packages/@dopt/const/src/client-side-messages.ts
+++ packages/@dopt/const/src/client-side-messages.ts
@@ -3,7 +3,7 @@ export const DELETE_FAIL_MSG = 'Could not delete block. Please try again.';
 export const RESET_FAIL_MSG = 'Could not reset journey. Please try again.';
 export const INVALID_EMAIL_MSG =
   'Your email address is not allowed. Please try a different one.';
-export const REGENERATE_FAIL_MSG =
+export const REGENERATE_APIKEY_FAIL_MSG =
   'Could not regenerate API key. Please try again.';
 export const JOURNEY_CTA_FAIL_MSG = 'Could not create journey';
 export const TRY_AGAIN_MSG = 'Please try again';
@@ -12,3 +12,7 @@ export const EXP_END_OF_INPUT_MSG =
 export const LOGOUT_FAIL_MSG = 'Could not log out';
 export const LOGIN_FAIL_MSG = 'Coud not log in. Please try again.';
 export const SIGNUP_FAIL_MSG = 'Could not sign up. Please try again.';
+export const GENERATE_APIKEY_FAIL_MSG =
+  'Could not generate API key. Please try again.';
+export const FETCH_APIKEY_FAIL_MSG =
+  'Could not find API key. Please try again.';
