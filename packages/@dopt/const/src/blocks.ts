212fdb4046d16ba71c2248969ebc0e2898651cf2
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2022-08-11 13:26:14 -0700

    fix(@dopt/flow): new blocks added to canvas are named incorrectly (#1044)

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -19,12 +19,17 @@ export const DEFAULT_BLOCK_NAMES: Record<BlockType, string> = {
   WebhookBlock: 'Webhook',
 };
 
+export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
+  EntryBlock: 'How users enter into the journey',
+  ModelBlock: 'Define state for an experience',
+  FinishBlock: 'Define when the journey is finished',
+  SwitchBlock: 'Switch',
+  BooleanBlock: 'Boolean',
+  EmailBlock: 'Email',
+  WaitBlock: 'Wait',
+  WebhookBlock: 'Webhook',
+};
+
 export const CHARACTER_LIMIT_NAME = 100;
 
 export const BLOCK_NAME_LENGTH_MSG = `Name can't be more than ${CHARACTER_LIMIT_NAME} characters`;
-
-export const ENTRY_BLOCK_DEFAULT_DESC = 'How users enter into the journey';
-
-export const MODEL_BLOCK_DEFAULT_DESC = 'Define state for an experience';
-
-export const FINISH_BLOCK_DEFAULT_DESC = 'Define when the journey is finished';
