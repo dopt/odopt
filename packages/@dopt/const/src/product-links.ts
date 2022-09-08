2160bfc5707b5fd2f4c41103f7146f89eb08f54d
Alex Chan <alexchantastic@users.noreply.github.com>
2022-09-08 10:43:47 -0700

    fix(@dopt/const): update docs links (#1222)

--- packages/@dopt/const/src/product-links.ts
+++ packages/@dopt/const/src/product-links.ts
@@ -1,28 +1,24 @@
-export const DOCS_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930';
+export const DOCS_LINK = 'https://docs.dopt.com/';
 
 export const REACT_SDK_LINK = 'https://react-sdk-docs.vercel.app/index.html';
 
-export const LEARN_MORE_JOURNEYS_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#49a58cfb0c744256a8000ad02fe9ca96';
+export const LEARN_MORE_FLOWS_LINK = 'https://docs.dopt.com/concepts#flows';
 
-export const LEARN_MORE_USERS_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#4c15c8cf816f4ea2aff5f18b29350634';
+export const LEARN_MORE_USERS_LINK = 'https://docs.dopt.com/concepts#users';
 
 export const LEARN_MORE_APIKEY_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#b717a53914744182ab0a4eacd809d236';
+  'https://docs.dopt.com/using-dopt#create-an-api-key';
 
 export const LEARN_MORE_USER_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#4c15c8cf816f4ea2aff5f18b29350634';
+  'https://docs.dopt.com/using-dopt#identify-users';
 
-export const ENTRY_CONDITION_LINK =
-  'https://switchboard.notion.site/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#f22d885d52eb4fce834b75ac10899df0';
+export const TARGETING_RULES_LINK =
+  'https://docs.dopt.com/concepts#start-blocks';
 
-export const RESET_USER_API_LINK =
-  'https://www.notion.so/switchboard/Dopt-Documentation-41e813b7b33f4c12acb867fe11b6f930#46d9a2d8b23746f1840475b4e28125d3';
+export const RESET_USER_API_LINK = 'https://docs.dopt.com/using-dopt#live';
 
 export const MAILTO_SUPPORT = 'mailto:support@dopt.com';
 
 export const WEBSITE_HOMEPAGE_LINK = 'https://www.dopt.com/';
 
-export const PRIVACY_POLICY_LINK = 'https://switchboard.cc/privacy';
+export const PRIVACY_POLICY_LINK = 'https://www.dopt.com/privacy';
