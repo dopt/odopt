9469f1272af198f19f75d55fbc3fb9209991a5f2
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2022-10-26 10:23:14 -0700

    feat(@dopt/logger): add styling to logger messages - browser (#1601)
    * feat(@dopt/logger): add styling to logger messages - browser
    
    * fix(@dopt/logger): addressing review comments

--- packages/@dopt/logger/src/style.ts
+++ packages/@dopt/logger/src/style.ts
@@ -3,7 +3,11 @@ import chalk, { type ChalkFunction } from 'chalk';
 
 const colors = tokens.colors.light;
 
-export const style: Record<string, ChalkFunction> = {
+const commonBrowserStyles = {
+  'font-weight': 'bold',
+  color: 'white',
+};
+export const nodeStyle: Record<string, ChalkFunction> = {
   prefix: chalk.bold.white.bgHex(colors.blurple500),
   debugTitle: chalk.bold.white.bgHex(colors.ultramarine500),
   errorTitle: chalk.bold.white.bgHex(colors.burntOrange500),
@@ -11,3 +15,43 @@ export const style: Record<string, ChalkFunction> = {
   warnTitle: chalk.bold.hex(colors.midnight500).bgHex(colors.sunGlow500),
   infoTitle: chalk.bold.hex(colors.midnight500).bgHex(colors.sky500),
 };
+
+export const browserStyle: Record<string, () => string> = {
+  prefix: browserStyles({
+    ...commonBrowserStyles,
+    background: colors.blurple500,
+  }),
+  debugTitle: browserStyles({
+    ...commonBrowserStyles,
+    background: colors.ultramarine500,
+  }),
+  errorTitle: browserStyles({
+    ...commonBrowserStyles,
+    background: colors.burntOrange500,
+  }),
+  errorBody: browserStyles({
+    color: colors.burntOrange500,
+  }),
+  warnTitle: browserStyles({
+    ...commonBrowserStyles,
+    background: colors.sunGlow500,
+    color: colors.midnight500,
+  }),
+  infoTitle: browserStyles({
+    ...commonBrowserStyles,
+    background: colors.sky500,
+    color: colors.midnight500,
+  }),
+};
+
+function browserStyles(css: string | Record<string, unknown>) {
+  let cssString = '';
+  if (typeof css == 'string') {
+    cssString = 'color:' + css;
+  } else {
+    for (const key in css) {
+      cssString = cssString + key + ':' + css[key] + ';';
+    }
+  }
+  return () => cssString;
+}
