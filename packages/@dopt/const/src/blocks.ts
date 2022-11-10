919fbdc1a504de5efa9447ead8eea4aa11e9bcad
Carlos Santillana <carlos@dopt.com>
2022-11-09 17:07:13 -0800

    refactor(@app/schema): remove unused blocks (#1680)

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -1,31 +1,13 @@
-export type BlockType =
-  | 'BooleanBlock'
-  | 'SwitchBlock'
-  | 'EmailBlock'
-  | 'FinishBlock'
-  | 'EntryBlock'
-  | 'ModelBlock'
-  | 'WaitBlock'
-  | 'WebhookBlock';
+export type BlockType = 'FinishBlock' | 'EntryBlock' | 'ModelBlock';
 
 export const DEFAULT_BLOCK_NAMES: Record<BlockType, string> = {
   EntryBlock: 'Start',
   ModelBlock: 'Step',
   FinishBlock: 'Finish',
-  SwitchBlock: 'Switch',
-  BooleanBlock: 'Boolean',
-  EmailBlock: 'Email',
-  WaitBlock: 'Wait',
-  WebhookBlock: 'Webhook',
 };
 
 export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
   EntryBlock: 'How users enter into the flow',
   ModelBlock: 'Define state for an experience',
   FinishBlock: 'Define when the flow is finished',
-  SwitchBlock: 'Switch',
-  BooleanBlock: 'Boolean',
-  EmailBlock: 'Email',
-  WaitBlock: 'Wait',
-  WebhookBlock: 'Webhook',
 };
