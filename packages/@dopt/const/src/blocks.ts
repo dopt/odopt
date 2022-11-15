a2f8c12a41faab9e28e89ecc0325981a70a0d7b6
Carlos Santillana <carlos@dopt.com>
2022-11-14 17:45:53 -0800

    feat(@blocks/schema): add state to base block + fix typeerrors from adding set block (#1701)
    * feat(@blocks/schema): add state to base block
    
    * chore(@blocks/service): perform dual write to prevent stalenes
    
    * fix(@blocks/service): small fixes
    
    * fix(@blocks/service): add set block integration
    
    only messy because we don't have STAG yet
    
    * fix(@app/client): typeerrors
    
    * chore(@dopt/const): add comment
    
    * fix(@blocks/service): peer reivew

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -1,13 +1,20 @@
-export type BlockType = 'FinishBlock' | 'EntryBlock' | 'ModelBlock';
+export type BlockType =
+  | 'FinishBlock'
+  | 'EntryBlock'
+  | 'ModelBlock'
+  | 'SetBlock';
 
 export const DEFAULT_BLOCK_NAMES: Record<BlockType, string> = {
   EntryBlock: 'Start',
   ModelBlock: 'Step',
   FinishBlock: 'Finish',
+  SetBlock: 'Set',
 };
 
 export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
   EntryBlock: 'How users enter into the flow',
   ModelBlock: 'Define state for an experience',
   FinishBlock: 'Define when the flow is finished',
+  // TODO: Replace this with something good. just put stuff to fix type issues
+  SetBlock: 'Groups together multiple model blocks',
 };
