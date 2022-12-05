1d6ddd06a2ac1e3118b33a60887038b04fdb9937
Maria Orias <102619044+mariaorias@users.noreply.github.com>
2022-12-05 09:05:40 -0800

    feat(@dopt/blocks): adding group components (#1761)
    * feat(@dopt/blocks): adding group components
    
    * refactor(@app/client): clean up
    
    * fix(@app/client): fix lockfile

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -8,13 +8,12 @@ export const DEFAULT_BLOCK_NAMES: Record<BlockTable, string> = {
   EntryBlock: 'Start',
   ModelBlock: 'Step',
   FinishBlock: 'Finish',
-  SetBlock: 'Set',
+  SetBlock: 'Group',
 };
 
 export const DEFAULT_BLOCK_DESC: Record<BlockTable, string> = {
   EntryBlock: 'How users enter into the flow',
   ModelBlock: 'Define state for an experience',
   FinishBlock: 'Define when the flow is finished',
-  // TODO: Replace this with something good. just put stuff to fix type issues
-  SetBlock: 'Groups together multiple model blocks',
+  SetBlock: 'Groups steps together',
 };
