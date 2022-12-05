a269c71b29e81bcabcfe3cea1207af097206995b
Carlos Santillana <carlos@dopt.com>
2022-12-05 13:57:15 -0800

    feat(@blocks/service): create flow intents (#1754)
    * feat(@blocks/service): v1 flow intents work
    
    * feat(@blocks/service): can now emit flow state
    
    * feat(@blocks/service): uses stag for flow intents now
    
    not completely tested yet
    
    * feat(@blocks/service): flow works now with stag
    
    just need to clean up a bit
    
    * chore(@blocks/service): small fixes + clean up

--- packages/@dopt/block-types/src/block/index.ts
+++ packages/@dopt/block-types/src/block/index.ts
@@ -6,7 +6,7 @@ import { Model } from './model';
 export * from './entry';
 export * from './end';
 export * from './model';
-
+export { BlockState } from './base';
 export type BlockTypes = End['type'] | Entry['type'] | Model['type'];
 
 export const Block = Type.Union([Model, Entry, End]);
