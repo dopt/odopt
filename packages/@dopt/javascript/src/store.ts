4d3f3eddc390809e0e5ca5452d48e36b38cd2baa
Joe McKenney <joe@switchboard.cc>
2022-12-09 11:26:29 -0800

    chore(@dopt/javascript): updates given v1 Blocks API changes (#1788)
    * feat(@app/client): integrate dopt-on-dopt console for testing this work
    
    * chore(@dopt/javascript): update js sdk given v1 api
    
    * chore(@dopt/javascript): fixes in top-level sdk and flow object
    
    * fix(@app/client): disable console growth (used for testing / pulled in from another PR)
    
    * chore(@app/client): remove console growth used for testing
    
    * refactor(@dopt/javascript): expose additional methods on javascript sdk
    
    * fix(@dopt/javascript): add error handling to cover type errors that were being ignored
    
    * fix(@dopt/javascript): auto-start flow if not started, fix bugs in socket subscription

--- packages/@dopt/javascript/src/store.ts
+++ packages/@dopt/javascript/src/store.ts
@@ -1,6 +1,11 @@
 import create from 'zustand/vanilla';
 import { subscribeWithSelector } from 'zustand/middleware';
 
-import type { Blocks } from '@dopt/javascript-common';
+import { Mercator } from '@dopt/mercator';
 
-export const store = create(subscribeWithSelector<Blocks>(() => ({})));
+import type { Blocks, Flows } from '@dopt/javascript-common';
+
+export const blockStore = create(subscribeWithSelector<Blocks>(() => ({})));
+export const flowStore = create(
+  subscribeWithSelector<Flows>(() => new Mercator())
+);
