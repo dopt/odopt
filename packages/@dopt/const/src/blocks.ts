ceb3644fdc0c784bf05c847a189bddda2bad9ab7
Carlos Santillana <carlos@dopt.com>
2022-12-13 08:45:05 -0800

    refactor(DOPT-1966): Add non-null constraints to app and blocks service (#1810)
    * feat(@blocks/service): tighten constraints for blocks db
    
    make type and flowId non-null
    
    * feat(@app/client): add type to UI and send to BE
    
    * feat(@app/server): add protection from bad input
    
    * refactor(@app/client): use type instead of table
    
    * refactor(@blocks/schema): remove element type
    
    * chore(@app/server): fix lint
    
    * refactor(@app/client): small clen ups

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -4,18 +4,31 @@ export type BlockTable =
   | 'ModelBlock'
   | 'SetBlock';
 
-export type BlockType = 'Entry' | 'Model' | 'Finish' | 'Set' | 'Element';
+export type BlockType = 'Entry' | 'Model' | 'Finish' | 'Set';
 
-export const DEFAULT_BLOCK_NAMES: Record<BlockTable, string> = {
-  EntryBlock: 'Start',
-  ModelBlock: 'Step',
-  FinishBlock: 'Finish',
-  SetBlock: 'Group',
+export const DEFAULT_BLOCK_NAMES: Record<BlockType, string> = {
+  Entry: 'Start',
+  Model: 'Step',
+  Finish: 'Finish',
+  Set: 'Group',
 };
 
-export const DEFAULT_BLOCK_DESC: Record<BlockTable, string> = {
-  EntryBlock: 'How users enter into the flow',
-  ModelBlock: 'Define state for an experience',
-  FinishBlock: 'Define when the flow is finished',
-  SetBlock: 'Groups steps together',
+export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
+  Entry: 'How users enter into the flow',
+  Model: 'Define state for an experience',
+  Finish: 'Define when the flow is finished',
+  Set: 'Groups steps together',
 };
+
+export function convertTypeToTable(type: BlockType): BlockTable {
+  switch (type) {
+    case 'Entry':
+      return 'EntryBlock';
+    case 'Finish':
+      return 'FinishBlock';
+    case 'Model':
+      return 'ModelBlock';
+    case 'Set':
+      return 'SetBlock';
+  }
+}
