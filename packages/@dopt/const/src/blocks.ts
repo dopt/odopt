3517e65e2f161c477810b5dd6c87ab4b7d5468c9
Carlos Santillana <carlos@dopt.com>
2022-12-20 08:13:59 -0800

    feat(@dopt/block-types): introduce set block (#1852)
    * feat(@dopt/block-types): introduce set block
    
    use across our repo
    
    * refactor(dopt): use const instead of blockType strings
    
    * chore(dopt): version bumps

--- packages/@dopt/const/src/blocks.ts
+++ packages/@dopt/const/src/blocks.ts
@@ -1,15 +1,14 @@
+import { BlockTypes, BLOCK_TYPES } from '@dopt/block-types';
 export type BlockTable = 'finish' | 'entry' | 'model' | 'set';
 
-export type BlockType = 'entry' | 'model' | 'finish' | 'set';
-
-export const DEFAULT_BLOCK_NAMES: Record<BlockType, string> = {
+export const DEFAULT_BLOCK_NAMES: Record<BlockTypes, string> = {
   entry: 'Start',
   model: 'Step',
   finish: 'Finish',
   set: 'Group',
 };
 
-export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
+export const DEFAULT_BLOCK_DESC: Record<BlockTypes, string> = {
   entry: 'How users enter into the flow',
   model: 'Define state for an experience',
   finish: 'Define when the flow is finished',
@@ -18,15 +17,15 @@ export const DEFAULT_BLOCK_DESC: Record<BlockType, string> = {
 
 // Not very useful right now but in the future this mapping is needed
 // and its scary to just assume that types and tables are 1:1 in code
-export function convertTypeToTable(type: BlockType): BlockTable {
+export function convertTypeToTable(type: BlockTypes): BlockTable {
   switch (type) {
-    case 'entry':
+    case BLOCK_TYPES.entry:
       return 'entry';
-    case 'finish':
+    case BLOCK_TYPES.finish:
       return 'finish';
-    case 'model':
+    case BLOCK_TYPES.model:
       return 'model';
-    case 'set':
+    case BLOCK_TYPES.set:
       return 'set';
   }
 }
