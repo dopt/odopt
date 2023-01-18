bff949b528edad41a48600cb7f1308d05fd74e2b
Carlos Santillana <carlos@dopt.com>
2023-01-18 15:47:54 -0800

    docs(@dopt/react): add typedocs and readme for groups (#1940)
    * docs(@dopt/react): add typedocs and readme for groups

---
+++ packages/@dopt/react/src/with-unordered-group.tsx
@@ -0,0 +1,33 @@
+import type { Set } from '@dopt/block-types';
+
+/**
+ * A React HOC for accessing group block state and methods corresponding
+ * to an intent-based API for maniuplating said state.
+ *
+ * @example
+ * ```tsx
+ *  import { withUnorderedGroup } from '@dopt/react';
+ *  import { WelcomeModal } from './welcome-modal';
+ *
+ *  export function Application() {
+ *    const WelcomeModalWithDopt = withUnorderedGroup(WelcomeChecklist, 'j0zExxZDVKCPXPzB2ZgpW');
+ *    return (
+ *      <main>
+ *        <WelcomeModalWithDopt />
+ *      </main>
+ *    );
+ *  }
+ * ```
+ *
+ * @param Component - the React component to inject Dopt props into
+ * @param uid - {@link Set['uid']}
+ * @returns The original component with {@link Set | set} and {@link BlockIntentions | intent}
+ * props injected in
+ *
+ */
+export function withOrderedGroup<T>(
+  Component: React.ComponentType<T>,
+  uid: Set['uid']
+) {
+  throw new Error('Not Implemented');
+}
