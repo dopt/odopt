94659f92bd0b87af7953f483d4d094e2c056e2aa
Joe McKenney <joe@switchboard.cc>
2023-01-19 10:52:27 -0800

    fix(@examples/*): update paths in production to assets in public (#1950)
    * fix(@examples/kanban): production builds utilize base path for referencing static assets in public
    
    * fix(@examples/getting-started-checklist): update paths to assets in /public in production

--- apps/@examples/getting-started-checklist/src/charts/most-active-users.tsx
+++ apps/@examples/getting-started-checklist/src/charts/most-active-users.tsx
@@ -3,7 +3,7 @@ import { Chart } from './chart';
 export function MostActiveUsers() {
   return (
     <Chart title="Most active users">
-      <img src="/charts/most-active-users.svg" />
+      <img src={`${import.meta.env.BASE_URL}charts/most-active-users.svg`} />
     </Chart>
   );
 }
