e70665c12c68e68aaf4fcbcfa9a21aa10ab42c2d
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2023-01-23 17:26:44 +0530

    feat(@app/client): onboarding groups card with seed for the same (#1966)

--- packages/@dopt/const/src/onboarding.ts
+++ packages/@dopt/const/src/onboarding.ts
@@ -2,6 +2,6 @@ export const WELCOME_MODAL_ID = 'ugcsZI-SB1ax7mbP0tXIg';
 export const FLOWS_CARD_ID = 'elNRhGd3bTeXUCBm3npbI';
 export const USERS_CARD_ID = 'mX1Ds_7vCoA_9_iPtii2s';
 //TODO: ADD this in onboarding
-export const GROUPS_CARD_ID = 'mX1Ds_7vCoA_9_iPtii2s';
+export const GROUPS_CARD_ID = 'q9k9XVPCGUIl6YTSfq1jq';
 export const ENV_CARD_ID = 'OFDJ4A9uRrUBLObFtc3xG';
 export const GUARD_STEP_ID = 'J9Gva7bfFsYPuEjLh328E';
