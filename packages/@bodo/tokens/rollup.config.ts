7e5952009631c005a5da845c28cfdd5cfd25a280
Joe McKenney <joe@switchboard.cc>
2023-01-25 14:27:21 -0800

    feat(@dopt/rollup): add a shared rollup configuration (#1985)
    * refactor(@dopt/react): to use @dopt/rollup for rollup configuration
    
    * refactor(@dopt/javascript): uses @dopt/rollup
    
    * fix(@dopt/users-javascript-client): use @dopt/rollup for rollup config
    
    * refactor(@dopt/logger): uses @dopt/rollup for rollup configuration
    
    * refactor(@dopt/javascript-common): uses @dopt/rollup for rollup configuration
    
    * refactor(@bodo/tokens): uses @dopt/rollup for rollup configuration
    
    * fix(@dopt/javascript): remove unneeded config
    
    * feat(@dopt/rollup): add two CLIs and utilize them in the packages that use @dopt/rollup
    
    * refactor(@dopt/rollup): move to one shared config && fix warnings about external deps
    
    * fix(@dopt/rollup): add tsup dep / reorganize deps
    
    * fix(@dopt/react): got a little overzealous with sed - add back doc gen
    
    * chore(@dopt/rollup): fix deps
    
    * fix(dopt): fix @dopt/react and @test/kanban tests
    
    * chore(dopt): add @types/node to packages that need it

--- packages/@bodo/tokens/rollup.config.ts
+++ packages/@bodo/tokens/rollup.config.ts
@@ -1,34 +1,5 @@
-import typescript from '@rollup/plugin-typescript';
-import externalizePeerDeps from 'rollup-plugin-peer-deps-external';
-import dts from 'rollup-plugin-dts';
+import { rollup } from '@dopt/rollup';
 
-import pkg from './package.json' assert { type: 'json' };
-
-const config = [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        file: pkg.exports['.'].import,
-        format: 'esm',
-      },
-      {
-        file: pkg.exports['.'].require,
-        format: 'cjs',
-      },
-    ],
-    plugins: [
-      typescript({
-        tsconfig: './tsconfig.json',
-      }),
-      externalizePeerDeps(),
-    ],
-  },
-  {
-    input: './dist/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts()],
-  },
-];
-
-export default config;
+export default rollup({
+  input: './src/index.ts',
+});
