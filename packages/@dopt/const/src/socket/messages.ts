b3372d81685588210f1b043e9e4d5e9c123f68bc
Chase <chase@dopt.com>
2023-01-30 14:16:33 -0800

    feat(@blocks/service): send flowSid and version in block to block emit (#2013)
    * feat(@blocks/service): send flowSid and version in block to block emit
    
    * fix(@blocks/service): update stale variable name

--- packages/@dopt/const/src/socket/messages.ts
+++ packages/@dopt/const/src/socket/messages.ts
@@ -1,6 +1,5 @@
 export const UPDATE_USER = 'User Updated';
-export const BLOCK_INTENT = 'Block Intent';
-export const FLOW_INTENT = 'Flow Intent';
+export const FLOW_INTENT_INTERNAL = 'Flow Intent Internal';
 export const NO_USER_PROVIDED_ERROR = 'No User Identifier Provided';
 export const NO_COOKIE_PROVIDED_ERROR = 'No Cookie Provided';
 export const NO_ENVIRONMENT_PROVIDED_ERROR = 'No Environment Provided';
