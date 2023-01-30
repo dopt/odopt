15fb028b64c82661758c955c89396c76f1ae2926
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2023-01-30 10:26:50 -0800

    feat(@app/client): implement group blocks for onboarding in Dopt (#2012)
    * feat(@app/client): implement group blocks for onboarding in Dopt
    
    * fix(@app/client): addressing review comments

--- packages/@dopt/const/src/onboarding.ts
+++ packages/@dopt/const/src/onboarding.ts
@@ -1,7 +1,6 @@
 export const WELCOME_MODAL_ID = 'ugcsZI-SB1ax7mbP0tXIg';
 export const FLOWS_CARD_ID = 'elNRhGd3bTeXUCBm3npbI';
 export const USERS_CARD_ID = 'mX1Ds_7vCoA_9_iPtii2s';
-//TODO: ADD this in onboarding
 export const GROUPS_CARD_ID = 'q9k9XVPCGUIl6YTSfq1jq';
 export const ENV_CARD_ID = 'OFDJ4A9uRrUBLObFtc3xG';
-export const GUARD_STEP_ID = 'J9Gva7bfFsYPuEjLh328E';
+export const GROUPS_SET_BLOCK_ID = 'deBOfeJC_AgjQhUy4VIo0';
