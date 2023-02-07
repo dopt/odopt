758e8a12f0dd0d8c90d3bb708c30c8e9e438bcdf
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2023-02-07 15:43:36 -0800

    feat(all): create and publish blocks-javascript-client (#2077)
    * feat(all): create and publish blocks-javascript-client
    
    * fix(all): addressing review comments
    
    * fix(@dopt/block-types): merge conlfict error
    
    * fix(all): type and test fixes
    
    * fix(all): final typefix
    
    * feat(all): adding blocks-javascript-client

--- packages/@dopt/block-types/src/block/webhook.ts
+++ packages/@dopt/block-types/src/block/webhook.ts
@@ -3,13 +3,16 @@ import { Base } from './base';
 
 export const WebhookTypeConst = 'webhook';
 export const WebhookType = Type.Literal(WebhookTypeConst);
-export const WebhookProps = Type.Object({
-  type: Type.Readonly(WebhookType),
-  method: Type.String(),
-  url: Type.String(),
-  headers: Type.Record(Type.String(), Type.String()),
-  body: Type.Object({}, { additionalProperties: true }),
-});
+export const WebhookProps = Type.Object(
+  {
+    type: Type.Readonly(WebhookType),
+    method: Type.String(),
+    url: Type.String(),
+    headers: Type.Record(Type.String(), Type.String()),
+    body: Type.Object({}, { additionalProperties: true }),
+  },
+  { $id: 'WebhookProps' }
+);
 
-export const Webhook = Type.Intersect([Base, WebhookProps]);
+export const Webhook = Type.Intersect([Base, WebhookProps], { $id: 'Webhook' });
 export type Webhook = Static<typeof Webhook>;
---
+++ packages/@dopt/blocks-api-manifest/README.md
@@ -0,0 +1,3 @@
+## BlocksApiManifest
+
+Some guidance for writing great READMEs can be found (here)[https://www.makeareadme.com/]
