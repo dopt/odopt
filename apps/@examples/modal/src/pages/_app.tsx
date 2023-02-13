95e4273d3c12fb6453fae7b7530890d889141f6c
Joe McKenney <joe@switchboard.cc>
2023-02-13 13:48:20 -0800

    feat(@examples/modal): scaffold out modal example package (#2136)
    * feat(@examples/modal): scaffold out modal example package
    
    * chore(dopt): changeset + start package version at 0.0.0

---
+++ apps/@examples/modal/src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { DoptProvider } from '@dopt/react';
+import { useIdentifyUser } from '@/hooks';
+
+export function App() {
+  const userId = useIdentifyUser({
+    company: 'Dopt',
+    role: 'admin',
+    inTrial: true,
+  });
+
+  useEffect(() => {
+    if (userId) {
+      if (document.referrer !== '') {
+        window.parent.postMessage(
+          JSON.stringify({ userId }),
+          document.referrer
+        );
+      }
+    }
+  }, [userId]);
+
+  return (
+    <DoptProvider
+      apiKey={import.meta.env.VITE_DOPT_BLOCKS_API_KEY}
+      userId={userId}
+      flowVersions={{ modal: 1 }}
+    >
+      <>Checklist Example</>
+    </DoptProvider>
+  );
+}
