992741adf195c259010f99b3f24839d1d81884e4
Joe McKenney <joe@switchboard.cc>
2023-02-16 12:05:06 -0800

    feat(@examples/embedded-tip): implement the example (#2177)
    * feat(@examples/embedded-tip): add skeletons
    
    * feat(@examples/embedded-tip): initial pass at structure and styles
    
    * feat(@examples/embedded-tip): wire up to dopt

---
+++ apps/@examples/embedded-tip/src/components/skeletons/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <svg viewBox="0 0 800 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="128" height="42" rx="8" fill="#F1F3F5" />
+      <rect x="618" width="42" height="42" rx="8" fill="#F1F3F5" />
+      <rect x="672" y="9" width="128" height="24" rx="8" fill="#F1F3F5" />
+    </svg>
+  );
+}
