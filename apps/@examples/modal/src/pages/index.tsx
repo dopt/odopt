b91600a837ce4481b9a0e256bbdf5ece44fcbd40
Joe McKenney <joe@switchboard.cc>
2023-02-16 09:45:53 -0800

    feat(@examples/modal): implement the modal example (#2174)
    * feat(@examples/modal): copy over learnings from checklist / import svg skeleton
    
    * feat(@examples/modal): being building modal example
    
    * feat(@examples/modal): first pass at structure/styling
    
    * feat(@examples/modal): wire up the example to dopt
    
    * feat(@examples/modal): remove the close icon

---
+++ apps/@examples/modal/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { useBlock } from '@dopt/react';
+
+import { example } from '@/pages/index.css';
+import { Skeleton, Modal, Button } from '@/components';
+
+export function Example() {
+  const [modal, methods] = useBlock('3Q3wx6Zlki3ZbmBhPSgum');
+
+  return (
+    <div className={example}>
+      <Skeleton />
+      <Modal open={modal.state.active}>
+        <div>
+          This is a modal that can do anything from feature announcements to
+          upsells.
+        </div>
+        <Button color="pink" onClick={methods.complete}>
+          Got it
+        </Button>
+      </Modal>
+    </div>
+  );
+}
