87d857bb59a4344027333ce3e4c2da300c6364e3
Joe McKenney <joe@switchboard.cc>
2023-02-17 14:53:36 -0800

    feat(@examples/custom-textarea): implement the custom textarea example (#2190)
    * feat(@examples/custom-textarea): scaffold out custom textarea example
    
    * feat(@examples/custom-textarea): initial wiring to dopt
    
    * feat(@examples/custom-textarea): finish wiring up states
    
    * fix(@examples/custom-textarea): rename k8s config

---
+++ apps/@examples/custom-textarea/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { DoptProvider } from '@dopt/react';
+import { useIdentifyUser } from '@/hooks';
+
+import { Example } from '@/pages/';
+
+export function App() {
+  const userId = useIdentifyUser({
+    company: 'Dopt',
+    role: 'admin',
+    inTrial: true,
+  });
+
+  return (
+    <DoptProvider
+      apiKey={import.meta.env.VITE_DOPT_BLOCKS_API_KEY}
+      userId={userId}
+      flowVersions={{ 'custom-textarea': 1 }}
+    >
+      <Example />
+    </DoptProvider>
+  );
+}
