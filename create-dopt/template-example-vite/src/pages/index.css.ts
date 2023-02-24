5967901880aba453cce4ad1b34bf59bf3cb24e5a
Joe McKenney <joe@switchboard.cc>
2023-02-24 15:06:17 -0800

    feat(create-dopt): add template-example-vite + builder (#2254)
    * fix(create-dopt): move to repo root
    
    * feature(create-dopt): copy pre-existing example app into template as starting poing
    
    * feat(create-dopt): add example-vite template
    
    * feat(create-dopt): add builder for vite example
    
    * fix(create-dopt): but in example-vite builder with quoted replacements
    
    * fix(create-dopt): remove test run from example builder
    
    * feat(create-dopt): doesn't run if you have local unstaged changes

---
+++ create-dopt/template-example-vite/src/pages/index.css.ts
@@ -0,0 +1,13 @@
+import { style, globalStyle } from '@vanilla-extract/css';
+
+globalStyle('body', {
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif',
+});
+
+export const example = style({
+  display: 'flex',
+  gap: '16px',
+  justifyContent: 'center',
+  flexDirection: 'column',
+});
---
+++ create-dopt/template-example-vite/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { useBlock } from '@dopt/react';
+
+import { example } from '@/pages/index.css';
+
+export function Example() {
+  return <div className={example}></div>;
+}
