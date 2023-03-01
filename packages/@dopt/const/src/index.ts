e584f9d454cfb4228e7a37d50c0605a47e5e0513
Karthik Sethuraman <karthik@dopt.com>
2023-03-01 15:08:08 -0800

    feat(@blocks/service): add an optional header if an intent causes side effects (commits changes) (#2281)
    * feat(@blocks/service): return 202 success codes if no objects are changed
    
    if intents don't cause changed, return a 202 code
    
    * fix(@blocks/service): use an optional HTTP header for changes instead of status code
    
    rather than using a 202 code, us an optional HTTP header if and only if the intent handler has saved changes

--- packages/@dopt/const/src/index.ts
+++ packages/@dopt/const/src/index.ts
@@ -2,6 +2,7 @@ export * from './regexp';
 export * from './secrets';
 export * from './health-check';
 export * from './operations';
+export * from './headers';
 export * from './blocks';
 export * from './journey-moves';
 export * from './journeys';
