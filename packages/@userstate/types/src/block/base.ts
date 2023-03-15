31ea296fc932a92c7a3dcac1227ee4e7abc73187
Joe McKenney <joe@switchboard.cc>
2023-03-15 14:39:18 -0700

    feat(@userstate/{machine|types}): named edges and back edges (#2325)
    * feat(@dopt/block-types): lift active out of state, rename state to paths and make arbitrary k/v
    
    * feat(@dopt/block-types): refactor towards a more complete model of primitive block types
    
    * feat(@dopt/block-types): refactor towards a single edge type in block types package
    
    * refactor(@dopt/block-types): create state and transitions / simplify types
    
    * fix(@dopt/block-types): add back doc comment in set
    
    * refactor(@dopt/block-types): avoid deriving type for blocks given its usage for docs
    
    d
    
    * feat(@dopt/stag): refactor given block type changes
    
    * refactor(@dopt/block-types): completed is broken into entered/exited + transition => past tense
    
    * feat(@dopt/stag): refactor with enter/exit states
    
    * feat(@dopt/block-types): introduce/formalize element type
    
    * fix(@dopt/stag): create element node and utilize element type
    
    * feat(@dopt/stag): checkpoint refactor / index.test.ts passing
    
    * feat(@dopt/stag): fix more tests
    
    * test(@dopt/stag): all uncommented tests pass
    
    * fix(@dopt/stag): add back commente dout block changed tests
    
    * test(@dopt/stag): add named edge tests
    
    * test(@dopt/stag): tests for back edges
    
    * fix(@dopt/stag): remove stray console log
    
    * feat(@userstate/machine): @dopt/stag => @userstate/machine + @dopt/block-types => @userstate/types
    
    * refactor(@userstate/machine): refactor consumers + use @userstate/types for
    
    * fix(@userstate/types): add getDefaultBlockState implementation back
    
    * feat(@userstate/machine): add boolean node + unit tests
    
    * fix(@userstate/machine): fix typos / rename test file to be more clear
    
    * refactor(@userstate/machine): update var name in iterator for clarity
    
    * feat(@blocks/service): encapsulate v1 => v2 and v2 => v1 in stag pre/post processing
    
    * fix(@blocks/service): remove comments that acted as my checklist
    
    * fix(@blocks/service): make default block function exhausitve, update entered logic
    
    * fix(@userstate/machine): use type predicate/guard instead of cast
    
    * fix(@blocks/service): remove debugging code in postprocessor
    
    * fix(@blocks/service): rewrite pre- and post- processor, fix a bug with sets
    
    * fix(@userstate/machine): add changed sets if any elements in the set have changed
    
    * fix(@userstate/machine): fix tests now that we're also returning the set block
    
    * fix(@userstate/machine): make edges strongly referential, pass-in a "default" intent
    
    * fix(@userstate/machine): fix circular set <> element references, fix set complete, fix finish block
    
    * fix(e2e/tests): undo changes to flowVersions in e2e tests
    
    * fix(@blocks/service): fix how @blocks/service determines when side effects will occur
    
    * fix(@test/react-javascript): flakey test
    
    ---------
    
    Co-authored-by: Karthik Sethuraman <karthik@dopt.com>
    Co-authored-by: Carlos Santillana <carlos@dopt.com>

---
+++ packages/@userstate/types/src/block/base.ts
@@ -0,0 +1,56 @@
+import { ObjectOptions, Static, Type } from '@sinclair/typebox';
+
+export const Base = Type.Object({
+  kind: Type.Literal('block'),
+  uid: Type.String(),
+  sid: Type.String(),
+  version: Type.Number(),
+  state: Type.Object({
+    active: Type.Boolean(),
+    entered: Type.Boolean(),
+    exited: Type.Boolean(),
+  }),
+});
+export type Base = Static<typeof Base>;
+
+export const Empty = Type.Object({
+  ...Base.properties,
+  transitioned: Type.Object({}),
+});
+export type Empty = Static<typeof Empty>;
+
+export const Unary = Type.Object({
+  ...Base.properties,
+  transitioned: Type.Object({
+    default: Type.Boolean(),
+  }),
+});
+export type Unary = Static<typeof Unary>;
+
+export const Binary = Type.Object({
+  ...Base.properties,
+  transitioned: Type.Object({
+    true: Type.Boolean(),
+    false: Type.Boolean(),
+  }),
+});
+export type Binary = Static<typeof Binary>;
+
+export const Nary = Type.Object({
+  ...Base.properties,
+  transitioned: Type.Record(Type.String(), Type.Boolean()),
+});
+export type Nary = Static<typeof Nary>;
+
+export const extend = (
+  primitive: typeof Empty | typeof Unary | typeof Binary | typeof Nary,
+  properties: Parameters<typeof Type.Object>[0],
+  options: ObjectOptions
+) =>
+  Type.Object(
+    {
+      ...primitive.properties,
+      ...properties,
+    },
+    options
+  );
