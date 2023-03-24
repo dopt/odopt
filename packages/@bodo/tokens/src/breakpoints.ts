7d3651f3dcc24e6fb2f78892483a21713fab2553
Alex Chan <alexchantastic@users.noreply.github.com>
2023-03-24 14:42:18 -0700

    refactor(dopt): centralize token access in @www/app and @blog/app (#2440)

--- packages/@bodo/tokens/src/breakpoints.ts
+++ packages/@bodo/tokens/src/breakpoints.ts
@@ -1,9 +1,9 @@
 export const breakpoints = {
-  sm: 540,
-  md: 768,
-  lg: 992,
-  xl: 1200,
-  '2xl': 1400,
+  sm: '540',
+  md: '768',
+  lg: '992',
+  xl: '1200',
+  '2xl': '1400',
 } as const;
 
 export type Breakpoint = keyof typeof breakpoints;
