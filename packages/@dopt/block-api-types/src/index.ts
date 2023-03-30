9fa42050733ce1f61edb820a89096cb761c86639
Carlos Santillana <carlos@dopt.com>
2023-03-30 09:03:52 -0700

    feat(@blocks/api): add flow endpoints to new v2 blocks service (#2455)

--- packages/@dopt/block-api-types/src/index.ts
+++ packages/@dopt/block-api-types/src/index.ts
@@ -1,15 +1,31 @@
-import type {
+import {
   Model as UserStateTypeModel,
   Flow as UserStateTypeFlow,
   Fields as UserStateTypeFields,
 } from '@userstate/types';
+import { Type, Static } from '@sinclair/typebox';
 
-export type Flow = Omit<UserStateTypeFlow, 'blocks'>;
-export type Model = Omit<UserStateTypeModel, 'fields'>;
+export const Model = UserStateTypeModel;
+export type Model = Static<typeof Model>;
 
+export const Fields = UserStateTypeFields;
 export type Fields = UserStateTypeFields;
 
+export const Block = Model;
 export type Block = Model;
+
+export const Flow = Type.Object(
+  {
+    ...UserStateTypeFlow.properties,
+    blocks: Type.Optional(Type.Array(Block)),
+  },
+  { $id: 'Flow' }
+);
+export type Flow = Static<typeof Flow>;
+
+export const BLOCK_TYPES = {
+  model: Model.properties.type.const,
+} as const;
 
 export function convert(block: UserStateTypeModel): Block {
   return block;
