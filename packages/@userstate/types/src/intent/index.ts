7cf1879da24d3022e3743c6906825b74c7779d4d
Carlos Santillana <carlos@dopt.com>
2023-03-30 09:03:52 -0700

    feat(@blocks/api): add flow endpoints to new v2 blocks service (#2455)

--- packages/@userstate/types/src/intent/index.ts
+++ packages/@userstate/types/src/intent/index.ts
@@ -2,11 +2,11 @@ import { Type, Static } from '@sinclair/typebox';
 
 export const FlowIntent = Type.Union(
   [
-    Type.Literal('complete', {
-      $id: 'complete_intent_flow',
-      title: 'Complete_Intent',
+    Type.Literal('finish', {
+      $id: 'finish_intent_flow',
+      title: 'Finish_Intent',
     }),
-    Type.Literal('exit', { $id: 'exit_intent', title: 'Exit_Intent' }),
+    Type.Literal('stop', { $id: 'stop_intent', title: 'Stop_Intent' }),
     Type.Literal('reset', { $id: 'reset_intent', title: 'Reset_Intent' }),
     Type.Literal('start', { $id: 'start_intent', title: 'Start_Intent' }),
   ],
