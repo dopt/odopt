b62ee8b37571e8b49211ef2d34077ab262387917
Karthik Sethuraman <karthik@dopt.com>
2023-04-07 11:47:37 -0700

    refactor(dopt/dopt): publish block-api-types and userstate/types, refactor blocks/api (#2505)
    * refactor(dopt/dopt): publish block-api-types and userstate/types, clean up blocks/*
    
    split out a subset of #2494 to simplify the change
    
    * fix(@blocks/api): fix typing by casting transitions
    
    can cast transitions because fastify schema guarantees minItems 1

--- packages/@dopt/block-api-types/src/index.ts
+++ packages/@dopt/block-api-types/src/index.ts
@@ -2,7 +2,10 @@ import {
   Model as UserStateTypeModel,
   Flow as UserStateTypeFlow,
   Fields as UserStateTypeFields,
+  Field as UserStateTypeField,
+  FlowIntent as UserStateTypeFlowIntent,
 } from '@userstate/types';
+
 import { Type, Static } from '@sinclair/typebox';
 
 export const Model = UserStateTypeModel;
@@ -11,8 +14,14 @@ export type Model = Static<typeof Model>;
 export const Fields = UserStateTypeFields;
 export type Fields = UserStateTypeFields;
 
+export const Field = UserStateTypeField;
+export type Field = UserStateTypeField;
+
 export const Block = Model;
 export type Block = Model;
+
+export const FlowIntent = UserStateTypeFlowIntent;
+export type FlowIntent = UserStateTypeFlowIntent;
 
 export const Flow = Type.Object(
   {
@@ -21,6 +30,7 @@ export const Flow = Type.Object(
   },
   { $id: 'Flow' }
 );
+
 export type Flow = Static<typeof Flow>;
 
 export const BLOCK_TYPES = {
@@ -30,8 +40,8 @@ export const BLOCK_TYPES = {
 export const Transitions = Type.Object({
   transitions: Type.Array(Type.String(), { minItems: 1, uniqueItems: true }),
 });
-export type Transitions = Static<typeof Transitions>;
+export type Transitions = {
+  transitions: [string, ...string[]];
+};
 
-export function convert(block: UserStateTypeModel): Block {
-  return block;
-}
+export const INTENT_SIDE_EFFECT_HEADER = 'X-Dopt-Intent-Side-Effects';
