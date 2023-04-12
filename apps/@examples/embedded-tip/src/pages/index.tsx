9fef8fe4bb88f4cb10010514c5be7f3c0abbe576
Karthik Sethuraman <karthik@dopt.com>
2023-04-12 13:32:22 -0700

    feat(@examples/*): update examples without groups to point to v2 sdk (#2556)
    also, add sids and bump up version numbers (yay!)

--- apps/@examples/embedded-tip/src/pages/index.tsx
+++ apps/@examples/embedded-tip/src/pages/index.tsx
@@ -1,20 +1,20 @@
-import { useBlock } from '@dopt/react-old';
+import { useBlock } from '@dopt/react';
 
 import { example, embeddedTipContainer } from '@/pages/index.css';
 
 import { Button, Skeletons, Notification } from '@/components/';
 
 export function Example() {
-  const [tip, methods] = useBlock('3QjH0QQ9L7V1FDbuEBzaH');
+  const [tip, transition] = useBlock<['default']>('embedded-tip.tip');
 
   return (
     <div className={example}>
       <Skeletons.Header />
       <div className={embeddedTipContainer}>
-        <Notification title={tip.getField('title', '')} open={tip.state.active}>
-          <div>{tip.getField('body')}</div>
-          <Button color="green" onClick={methods.complete}>
-            {tip.getField('button')}
+        <Notification title={tip.field('title', '')} open={tip.state.active}>
+          <div>{tip.field('body')}</div>
+          <Button color="green" onClick={() => transition('default')}>
+            {tip.field('button')}
           </Button>
         </Notification>
       </div>
