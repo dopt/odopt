be0fdb97fce37127b1267d4a6eb3c71b55c36baf
Karthik Sethuraman <karthik@dopt.com>
2023-04-13 11:06:33 -0700

    feat(@examples/*): refactor examples with unordered groups to use v2 sdks (#2571)
    * feat(@examples/hotspot): update hotspot example to use v2 sdk with sids
    
    * feat(@examples/embedded-contextual-education): update example to use v2 sdk with new sids
    
    * feat(@examples/checklist): update checklist example to use v2 sdks with new sids
    
    * feat(@examples/kanban): update kanban example to use v2 sdk with new sids
    
    * feat(@examples/getting-started-checklist): update examples to use v2 sdks and new sids
    
    * fix(@examples/checklist): clean up percentage true code since this is externally visible

--- apps/@examples/embedded-contextual-education/src/const/index.ts
+++ apps/@examples/embedded-contextual-education/src/const/index.ts
@@ -1,4 +1,3 @@
-export const GROUP = 'i2txq7OhO86vzQX8Oq1i8';
-export const LIBRARY_TIP = '75yCV9cpoWSTKJ_oyaDP9';
-export const COLLECTIONS_TIP = 'pMlCIh8f68iGIf19la3_d';
-export const ANALYTICS_TIP = 'X5WZOKeYjWiGFEzjZiNjV';
+export const LIBRARY_TIP = 'embedded-contextual-education.library-tip';
+export const COLLECTIONS_TIP = 'embedded-contextual-education.collections-tip';
+export const ANALYTICS_TIP = 'embedded-contextual-education.analytics-tip';
