cd4f711d57686d26409d3767c8d15ac4b15f5d3a
Joe McKenney <joe@switchboard.cc>
2023-06-09 15:19:03 -0700

    feat(@dopt/react-tour): initial scaffolding of tour item components (#2940)
    * chore(@dopt/react-tour): checkpoint basic position of portal content
    
    * chore(@dopt/react-tour): expose placement and alignment
    
    * chore(@dopt/react-tour): fake styling
    
    * chore(@dopt/react-tour): fix types
    
    * chore(@dopt/react-tour): some basic styling in the sandbox for testing
    
    * chore(@dopt/react-tour): scaffold our the various tour item components
    
    * chore(@dopt/react-tour): scaffold out button components
    
    * chore(dopt): bump lockfile
    
    * test(@dopt/react-tour-sandbox): add a second tour step and wire
    
    * chore(@dopt/react-tour-sandbox): formatting

---
+++ components/@dopt/react/tour/src/tour.tsx
@@ -0,0 +1,473 @@
+import * as classes from './styles';
+import { classNameRoot } from './const';
+
+import {
+  Children,
+  ForwardedRef,
+  PropsWithChildren,
+  ReactElement,
+  cloneElement,
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  forwardRef,
+  MouseEventHandler,
+} from 'react';
+
+import { type ComponentPropsWithRef } from '@dopt/react-component';
+
+import {
+  cls,
+  type StyleProps,
+  ThemeContext,
+  getThemeClassName,
+  useTheme,
+} from '@dopt/react-theme';
+
+import type { TourItem } from '@dopt/semantic-data-layer-tour';
+import { Portal } from '@dopt/react-portal';
+import { usePosition, Placement, Alignment } from '@dopt/react-utilities';
+
+export interface TourItemContext {
+  anchor: HTMLDivElement | null;
+  setAnchor: React.Dispatch<React.SetStateAction<HTMLDivElement | null>>;
+}
+
+const TourItemContext = createContext<TourItemContext>({
+  anchor: null,
+  setAnchor: () => {},
+});
+
+export interface TourItemProps extends PropsWithChildren, StyleProps {}
+
+function TourItem(props: TourItemProps) {
+  const { theme, children } = props;
+
+  const [anchor, setAnchor] = useState<TourItemContext['anchor']>(null);
+
+  return (
+    <TourItemContext.Provider
+      value={{
+        anchor,
+        setAnchor,
+      }}
+    >
+      <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    </TourItemContext.Provider>
+  );
+}
+
+export interface ContentProps
+  extends ComponentPropsWithRef<'section'>,
+    StyleProps {}
+
+const contentClassName = `${classNameRoot}__content` as const;
+
+function TourItemContent(
+  props: ContentProps,
+  ref?: ForwardedRef<HTMLDivElement>
+) {
+  const { css, theme: injectedTheme, className, ...restProps } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return (
+    <section
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.content({ css }), theme],
+        }),
+        contentClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    />
+  );
+}
+
+export interface HeaderProps
+  extends ComponentPropsWithRef<'header'>,
+    StyleProps {}
+
+const headerClassName = `${classNameRoot}__header` as const;
+
+function TourItemHeader(props: HeaderProps, ref?: ForwardedRef<HTMLElement>) {
+  const { css, theme: injectedTheme, className, ...restProps } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return (
+    <header
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.header({ css }), theme],
+        }),
+        headerClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    />
+  );
+}
+
+export interface TitleProps extends ComponentPropsWithRef<'h1'>, StyleProps {}
+
+const titleClassName = `${classNameRoot}__title` as const;
+
+function TourItemTitle(
+  props: TitleProps,
+  ref?: ForwardedRef<HTMLHeadingElement>
+) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    children,
+    ...restProps
+  } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return children == undefined ? null : (
+    <h1
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.title({ css }), theme],
+        }),
+        titleClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export interface DismissIconProps
+  extends ComponentPropsWithRef<'button'>,
+    StyleProps {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const dismissIconClassName = `${classNameRoot}__dismiss-icon` as const;
+
+function TourItemDismissIcon(
+  props: DismissIconProps,
+  ref?: ForwardedRef<HTMLButtonElement>
+) {
+  const { css, theme: injectedTheme, className, onClick, ...restProps } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  const icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M18 6l-12 12"></path>
+      <path d="M6 6l12 12"></path>
+    </svg>
+  );
+
+  return (
+    <button
+      onClick={onClick}
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.dismissIcon({ css }), theme],
+        }),
+        dismissIconClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    >
+      {icon}
+    </button>
+  );
+}
+
+export interface TourItemAnchorProps extends StyleProps {
+  children: ReactElement;
+}
+
+function TourItemAnchor(props: TourItemAnchorProps) {
+  const { setAnchor } = useContext(TourItemContext);
+
+  const anchorRef = useRef(null);
+
+  setAnchor(anchorRef.current);
+
+  let anchor = Children.only(props.children);
+
+  return cloneElement(anchor, {
+    ref: anchorRef,
+  });
+}
+
+export interface TourPopoverProps
+  extends ComponentPropsWithRef<'div'>,
+    StyleProps {
+  open?: boolean;
+  placement?: Placement;
+  alignment?: Alignment;
+}
+
+const popoverClassName = `${classNameRoot}__popover` as const;
+
+function TourPopover(props: TourPopoverProps) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    children,
+    open,
+    placement = 'top',
+    alignment = 'left',
+    ...restProps
+  } = props;
+
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const { anchor } = useContext(TourItemContext);
+
+  const theme = useTheme(injectedTheme);
+
+  if (!open) {
+    return null;
+  }
+
+  const position = usePosition({
+    anchor,
+    element: popoverRef.current,
+    placement,
+    alignment,
+  });
+
+  return (
+    <Portal>
+      <div
+        style={{
+          top: position?.top,
+          left: position?.left,
+        }}
+        className={cls([
+          getThemeClassName({
+            theme,
+            className: [classes.popover({ css }), theme],
+          }),
+          popoverClassName,
+          className,
+        ])}
+        {...restProps}
+        ref={popoverRef}
+      >
+        {children}
+      </div>
+    </Portal>
+  );
+}
+
+export interface BodyProps
+  extends Omit<ComponentPropsWithRef<'div'>, 'title'>,
+    StyleProps {
+  children?: TourItem['body'];
+}
+
+const bodyClassName = `${classNameRoot}__body` as const;
+
+function TourItemBody(props: BodyProps, ref?: ForwardedRef<HTMLDivElement>) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    children,
+    ...restProps
+  } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return children == undefined ? null : (
+    <div
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.body({ css }), theme],
+        }),
+        bodyClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    >
+      {children}
+    </div>
+  );
+}
+
+export interface FooterProps
+  extends Omit<ComponentPropsWithRef<'footer'>, 'title'>,
+    StyleProps {}
+
+const footerClassName = `${classNameRoot}__footer` as const;
+
+function TourItemFooter(props: FooterProps, ref?: ForwardedRef<HTMLElement>) {
+  const { css, theme: injectedTheme, className, ...restProps } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return (
+    <footer
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.footer({ css }), theme],
+        }),
+        footerClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    />
+  );
+}
+
+export interface BackButtonProps
+  extends ComponentPropsWithRef<'button'>,
+    StyleProps {
+  children?: TourItem['backLabel'];
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const dismissButtonClassName = `${classNameRoot}__back-button` as const;
+
+function TourItemBackButton(
+  props: BackButtonProps,
+  ref?: ForwardedRef<HTMLButtonElement>
+) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    children,
+    onClick,
+    ...restProps
+  } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return children == undefined ? null : (
+    <button
+      onClick={onClick}
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.backButton({ css }), theme],
+        }),
+        dismissButtonClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    >
+      {children}
+    </button>
+  );
+}
+
+export interface NextButtonProps
+  extends ComponentPropsWithRef<'button'>,
+    StyleProps {
+  children?: TourItem['nextLabel'];
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const nextButtonClassName = `${classNameRoot}__next-button` as const;
+
+function TourItemNextButton(
+  props: NextButtonProps,
+  ref?: ForwardedRef<HTMLButtonElement>
+) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    children,
+    onClick,
+    ...restProps
+  } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  return children == undefined ? null : (
+    <button
+      onClick={onClick}
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: [classes.nextButton({ css }), theme],
+        }),
+        nextButtonClassName,
+        className,
+      ])}
+      {...restProps}
+      ref={ref}
+    >
+      {children}
+    </button>
+  );
+}
+
+const Root = TourItem;
+const Anchor = TourItemAnchor;
+const Popover = TourPopover;
+const Content = forwardRef(TourItemContent);
+const Header = forwardRef(TourItemHeader);
+const Title = forwardRef(TourItemTitle);
+const DismissIcon = forwardRef(TourItemDismissIcon);
+const Body = forwardRef(TourItemBody);
+const Footer = forwardRef(TourItemFooter);
+const BackButton = forwardRef(TourItemBackButton);
+const NextButton = forwardRef(TourItemNextButton);
+
+export {
+  Root,
+  Anchor,
+  Popover,
+  TourItem,
+  Content,
+  Header,
+  Title,
+  DismissIcon,
+  Body,
+  Footer,
+  BackButton,
+  NextButton,
+  Anchor as TourItemAnchor,
+  Popover as TourPopover,
+  Content as TourItemContent,
+  Header as TourItemHeader,
+  Title as TourItemTitle,
+  DismissIcon as TourItemDismissIcon,
+  Body as TourItemBody,
+  Footer as TourItemFooter,
+  BackButton as TourItemBackButton,
+  NextButton as TourItemNextButton,
+};
