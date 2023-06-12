941d144a9e1240e6a11a1ed634957cdf69261c0a
Alex Chan <alexchantastic@users.noreply.github.com>
2023-06-12 15:43:20 -0700

    feat(@dopt/react-tour): update styling (#2951)

--- components/@dopt/react/tour/src/styles.ts
+++ components/@dopt/react/tour/src/styles.ts
@@ -1,40 +1,201 @@
 import {} from '@stitches/core';
 import { theme } from '@dopt/react-theme';
 
-const { css } = theme;
+const { css, theme: vars } = theme;
 
-const tourItemRoot = css({
-  all: 'revert',
-});
-
-const tourItemAnchor = css({
-  all: 'revert',
-});
+const alignment = {
+  horizontal: {
+    start: {
+      '&:before, &:after': {
+        left: '$$arrowGap',
+      },
+    },
+    center: {
+      '&:before, &:after': {
+        right: '$0',
+        left: '$0',
+        marginRight: '$auto',
+        marginLeft: '$auto',
+      },
+    },
+    end: {
+      '&:before, &:after': {
+        right: '$$arrowGap',
+      },
+    },
+  },
+  vertical: {
+    start: {
+      '&:before, &:after': {
+        top: '$$arrowGap',
+      },
+    },
+    center: {
+      '&:before, &:after': {
+        top: '$0',
+        bottom: '$0',
+        marginTop: '$auto',
+        marginBottom: '$auto',
+      },
+    },
+    end: {
+      '&:before, &:after': {
+        bottom: '$$arrowGap',
+      },
+    },
+  },
+};
 
 const tourItemPopover = css({
+  $$arrowSize: vars.sizes[3],
+  $$arrowGap: vars.sizes[6],
   all: 'revert',
   position: 'absolute',
+  fontFamily: '$sans',
+  fontSize: '$base',
+  color: '$content',
+  lineHeight: '$base',
+  '&, & *': {
+    boxSizing: 'border-box',
+  },
+  '&:before, &:after': {
+    content: '',
+    position: 'absolute',
+    zIndex: 1,
+    width: '$$arrowSize',
+    height: '$$arrowSize',
+  },
+  '&:before': {
+    background: '$border',
+  },
+  '&:after': {
+    background: '$background',
+  },
+  variants: {
+    position: {
+      top: {
+        '&:before, &:after': {
+          top: 'calc(-$$arrowSize / 2)',
+          clipPath: 'polygon(50% 0, 100% 50%, 0 50%)',
+        },
+        '&:after': {
+          translate: `0 ${vars.borderWidths[1]}`,
+        },
+      },
+      right: {
+        '&:before, &:after': {
+          right: 'calc(-$$arrowSize / 2)',
+          clipPath: 'polygon(50% 100%, 100% 50%, 50% 0)',
+        },
+        '&:after': {
+          translate: `calc(${vars.borderWidths[1]} * -1) 0`,
+        },
+      },
+      bottom: {
+        '&:before, &:after': {
+          bottom: 'calc(-$$arrowSize / 2)',
+          clipPath: 'polygon(50% 100%, 0 50%, 100% 50%)',
+        },
+        '&:after': {
+          translate: `0 calc(${vars.borderWidths[1]} * -1)`,
+        },
+      },
+      left: {
+        '&:before, &:after': {
+          left: 'calc(-$$arrowSize / 2)',
+          clipPath: 'polygon(50% 100%, 0 50%, 50% 0)',
+        },
+        '&:after': {
+          translate: `${vars.borderWidths[1]} 0`,
+        },
+      },
+    },
+    alignment: {
+      start: {},
+      center: {},
+      end: {},
+    },
+  },
+  compoundVariants: [
+    {
+      position: 'top',
+      alignment: 'start',
+      css: alignment.horizontal.start,
+    },
+    {
+      position: 'top',
+      alignment: 'center',
+      css: alignment.horizontal.center,
+    },
+    {
+      position: 'top',
+      alignment: 'end',
+      css: alignment.horizontal.end,
+    },
+
+    {
+      position: 'bottom',
+      alignment: 'start',
+      css: alignment.horizontal.start,
+    },
+    {
+      position: 'bottom',
+      alignment: 'center',
+      css: alignment.horizontal.center,
+    },
+    {
+      position: 'bottom',
+      alignment: 'end',
+      css: alignment.horizontal.end,
+    },
+
+    {
+      position: 'right',
+      alignment: 'start',
+      css: alignment.vertical.start,
+    },
+    {
+      position: 'right',
+      alignment: 'center',
+      css: alignment.vertical.center,
+    },
+    {
+      position: 'right',
+      alignment: 'end',
+      css: alignment.vertical.end,
+    },
+
+    {
+      position: 'left',
+      alignment: 'start',
+      css: alignment.vertical.start,
+    },
+    {
+      position: 'left',
+      alignment: 'center',
+      css: alignment.vertical.center,
+    },
+    {
+      position: 'left',
+      alignment: 'end',
+      css: alignment.vertical.end,
+    },
+  ],
 });
 
 const tourItemContent = css({
   all: 'unset',
   position: 'relative',
   display: 'block',
-  maxWidth: '80vw',
-  padding: '$4',
+  width: 300,
+  padding: '$6',
   borderWidth: '$1',
   borderStyle: 'solid',
   borderColor: '$border',
   borderRadius: '$2',
-  boxShadow: '$2',
+  boxShadow: '$1',
   backgroundColor: '$background',
   '&:focus': { outline: 'none' },
-});
-
-const tourItemBody = css({
-  all: 'unset',
-  paddingTop: '$4',
-  paddingBottom: '$4',
 });
 
 const tourItemHeader = css({
@@ -42,6 +203,7 @@ const tourItemHeader = css({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'flex-start',
+  paddingBottom: '$2',
 });
 
 const tourItemTitle = css({
@@ -55,8 +217,6 @@ const tourItemDismissIcon = css({
   all: 'unset',
   padding: '$1',
   borderRadius: '$2',
-  alignSelf: 'start',
-  color: '$content',
   transition: 'background-color $ease',
   '& > svg': {
     display: 'block',
@@ -69,12 +229,18 @@ const tourItemDismissIcon = css({
   },
 });
 
+const tourItemBody = css({
+  all: 'unset',
+  display: 'block',
+});
+
 const tourItemFooter = css({
   all: 'unset',
   display: 'flex',
   justifyContent: 'flex-end',
   alignItems: 'flex-start',
   gap: '$2',
+  paddingTop: '$2',
 });
 
 const tourItemBackButton = css({
@@ -109,8 +275,6 @@ const tourItemNextButton = css({
 });
 
 export {
-  tourItemRoot,
-  tourItemAnchor,
   tourItemPopover,
   tourItemContent,
   tourItemHeader,
@@ -120,8 +284,6 @@ export {
   tourItemFooter,
   tourItemBackButton,
   tourItemNextButton,
-  tourItemRoot as root,
-  tourItemAnchor as anchor,
   tourItemPopover as popover,
   tourItemContent as content,
   tourItemHeader as header,
