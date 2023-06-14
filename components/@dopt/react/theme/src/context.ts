a779c8ee2416d677cf8d65333d6548ff13f00b60
Joe McKenney <joe@switchboard.cc>
2023-06-13 17:01:27 -0700

    feat(@dopt/core-theme): extract non-react theme into core pacakge (#2961)

--- components/@dopt/react/theme/src/context.ts
+++ components/@dopt/react/theme/src/context.ts
@@ -1,4 +1,4 @@
 import { createContext } from 'react';
-import type { StyleTheme } from './types';
+import type { StyleTheme } from '@dopt/core-theme';
 
 export const ThemeContext = createContext<StyleTheme>(undefined);
