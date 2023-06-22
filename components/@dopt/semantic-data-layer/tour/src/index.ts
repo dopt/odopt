f7687fc08463c07e69c763b131b40f9c437d6c89
Joe McKenney <joe@switchboard.cc>
2023-06-22 14:22:34 -0700

    feat(@dopt/react-tour): add useTourItem hook, wire up hooks, update interfaces (#3046)
    * feat(@dopt/react-tour): add useTourItem hook, wire up hooks, update interfaces
    
    * fix(@dopt/react-tour): add index to tour item interface

--- components/@dopt/semantic-data-layer/tour/src/index.ts
+++ components/@dopt/semantic-data-layer/tour/src/index.ts
@@ -1,16 +1,41 @@
 export interface Tour {
+  id: string;
+
   items: TourItem[];
+
+  active: boolean;
+
+  completed: boolean;
+  dismissed: boolean;
+
   complete: () => void;
+  dismiss: () => void;
+
+  get size(): number;
+
+  filter: (fn: (item: TourItem, i?: number) => boolean) => TourItem[];
+
+  count(fn: (item: TourItem, i?: number) => any): number;
+  count(type: 'completed' | 'incomplete'): number;
 }
 
 export interface TourItem {
   id: string;
+
+  index: number | null;
+
   title?: string | null;
   body?: string | null;
+
   nextLabel?: string | null;
   backLabel?: string | null;
+
   active: boolean;
+
+  completed: boolean;
+
   next: () => void;
   back: () => void;
+
   dismiss: () => void;
 }
