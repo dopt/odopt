41a1f04e3eed16e430547b8695ecdd083e5c4325
Karthik Sethuraman <karthik@dopt.com>
2023-06-26 16:30:17 -0400

    feat(@dopt/javascript): improves block and container abstractions, adds semantic accessors for modal, checklists, and tours (#3048)
    * feat(@dopt/javascript): adds headless modal accessor and creates container abstraction
    
    also, cleans up semantic-data-layer packages in preparation for using them in a public repository;
    this diff will also add checklist and tour accessors in upcoming commits.
    
    * feat(@dopt/javascript): update field typing + generic access
    
    * feat(@dopt/javascript): add semantic components to back modal, checklist, and tour experiences
    
    * refactor dopt.ts provider block creation to support these semantic components

---
+++ packages/@dopt/javascript/src/tour.ts
@@ -0,0 +1,122 @@
+import { Block, BlockProps } from './block';
+import { Container } from './container';
+import {
+  TourItem as TourItemInterface,
+  Tour as TourInterface,
+  FilterableField,
+  CountableField,
+} from '@dopt/semantic-data-layer-tour';
+
+/**
+ * A tour class obeys the tour interface
+ * specified in @dopt/semantic-data-layer-tour.
+ *
+ * It also extends the {@link Container} class.
+ *
+ * By extending the Container class, it also offers
+ * the `subscribe` method specified in {@link Container}.
+ * `subscribe` allows you to register a listener
+ * for any state change to a tour component or its
+ * tour item children.
+ */
+export class Tour extends Container<TourItem> implements TourInterface {
+  get id() {
+    return this.sid;
+  }
+  get active() {
+    return this.state.active;
+  }
+  get completed() {
+    return this.state.exited && !!this.transitioned.complete;
+  }
+  get dismissed() {
+    return this.state.exited && !!this.transitioned.dismiss;
+  }
+  complete() {
+    this.transition('complete');
+  }
+  dismiss() {
+    this.transition('dismiss');
+  }
+  get items() {
+    return this.children.sort((a, b) => (a.index || 0) - (b.index || 0));
+  }
+  get size() {
+    return this.childUids.length;
+  }
+  filter(on: FilterableField) {
+    return this.items.filter((item) => {
+      switch (on) {
+        case 'active':
+          return item.active;
+        case 'not-active':
+          return !item.active;
+        case 'completed':
+          return item.completed;
+        case 'not-completed':
+          return !item.completed;
+      }
+    });
+  }
+  count(on: CountableField) {
+    return this.filter(on).length;
+  }
+  protected childBlockCreator(props: BlockProps) {
+    return new TourItem({
+      ...props,
+      dismissTour: this.dismiss.bind(this),
+    });
+  }
+}
+
+export class TourItem
+  extends Block<['next', 'previous']>
+  implements TourItemInterface
+{
+  private dismissTour: () => void;
+
+  /**
+   * @internal
+   */
+  constructor({
+    dismissTour,
+    ...blockProps
+  }: BlockProps & { dismissTour: TourItem['dismissTour'] }) {
+    super(blockProps);
+    this.dismissTour = dismissTour;
+  }
+
+  get id() {
+    return this.sid;
+  }
+  get active() {
+    return this.state.active;
+  }
+  get completed() {
+    return this.state.exited && !!this.transitioned.next;
+  }
+  next() {
+    this.transition('next');
+  }
+  back() {
+    this.transition('previous');
+  }
+  dismiss() {
+    this.dismissTour();
+  }
+  get nextLabel() {
+    return this.field<string>('next-label');
+  }
+  get backLabel() {
+    return this.field<string>('back-label');
+  }
+  get title() {
+    return this.field<string>('title');
+  }
+  get body() {
+    return this.field<string>('body');
+  }
+  get index() {
+    return this.field<number>('display-index');
+  }
+}
