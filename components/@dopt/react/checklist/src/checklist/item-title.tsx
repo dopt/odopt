a51d0a1254e6fbd677c2aa8e22ebdac655803831
Joe McKenney <joe@switchboard.cc>
2023-06-28 13:29:16 -0700

    fix(@dopt/react-checklist): items are composed as opposed to configured (#3089)
    * refactor(@dopt/react-checklist): extract icons out of item in prepartion for exposing them to users
    
    * refactor(@dopt/react-checklist): break the item down into its individual components
    
    * refactor(@dopt/react-checklist): support composed version only for the launch
    
    * refactor(@dopt/react-checklist-sandbox): update checklist to be composed
    
    * fix(@dopt/react-checklist): finish moving back towards a fully composed solution

---
+++ components/@dopt/react/checklist/src/checklist/item-title.tsx
@@ -0,0 +1,63 @@
+import * as classes from '../styles';
+import { classNameRoot } from '../const';
+
+import {
+  forwardRef,
+  type ForwardedRef,
+  type ComponentPropsWithRef,
+} from 'react';
+
+import {
+  cls,
+  type StyleProps,
+  getThemeClassName,
+  useTheme,
+} from '@dopt/react-theme';
+import { ChecklistItem } from '@dopt/semantic-data-layer-checklist';
+
+export interface ChecklistItemTitleProps
+  extends Omit<ComponentPropsWithRef<'div'>, 'title'>,
+    StyleProps {
+  children: ChecklistItem['title'];
+  disabled?: boolean;
+}
+
+const itemClassName = `${classNameRoot}__item` as const;
+
+function ChecklistItemTitle(
+  props: ChecklistItemTitleProps,
+  ref?: ForwardedRef<HTMLDivElement>
+) {
+  const {
+    css,
+    theme: injectedTheme,
+    className,
+    disabled = false,
+    ...restProps
+  } = props;
+
+  const theme = useTheme(injectedTheme);
+
+  if (!props.children) {
+    return null;
+  }
+
+  return (
+    <div
+      className={cls([
+        getThemeClassName({
+          theme,
+          className: classes.itemTitle({
+            disabled,
+          }),
+        }),
+        `${itemClassName}-title`,
+      ])}
+      {...restProps}
+      ref={ref}
+    />
+  );
+}
+
+const ItemTitle = forwardRef(ChecklistItemTitle);
+export { ItemTitle, ItemTitle as ChecklistItemTitle };
