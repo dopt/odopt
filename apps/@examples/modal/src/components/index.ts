b1e0cf6d04f15f31f4232036a0dde4e8793841b1
Alex Chan <alexchantastic@users.noreply.github.com>
2023-07-13 14:46:16 -0700

    feat(dopt): add new modal examples (#3271)

--- apps/@examples/modal/src/components/index.ts
+++ apps/@examples/modal/src/components/index.ts
@@ -1,3 +1,1 @@
 export * from './skeleton';
-export * from './modal';
-export * from './button';
--- apps/@examples/modal/src/pages/index.tsx
+++ apps/@examples/modal/src/pages/index.tsx
@@ -1,23 +1,34 @@
-import { useBlock } from '@dopt/react';
+import { Skeleton } from '@/components';
+import Modal, { useModal } from '@dopt/react-modal';
 
-import { example } from '@/pages/index.css';
-import { Skeleton, Modal, Button } from '@/components';
+import './index.css';
 
-export function Example() {
-  const [modal, transition] = useBlock<['default']>('modal.modal');
+export function Home() {
+  // Access the modal data from the modal block in the flow
+  const modal = useModal('modal-component.heavy-teeth-ask');
 
   return (
-    <div className={example}>
+    <div className="home">
+      <Modal.Root active={modal.active} className="welcome-modal">
+        <Modal.Overlay />
+        <Modal.Content>
+          <Modal.Header>
+            <Modal.Title>{modal.title}</Modal.Title>
+            <Modal.DismissIcon onClick={modal.dismiss} />
+          </Modal.Header>
+          <Modal.Body>{modal.body}</Modal.Body>
+          <Modal.Footer>
+            <Modal.DismissButton onClick={modal.dismiss}>
+              {modal.dismissLabel}
+            </Modal.DismissButton>
+            <Modal.CompleteButton onClick={modal.complete}>
+              {modal.completeLabel}
+            </Modal.CompleteButton>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal.Root>
+
       <Skeleton />
-      <Modal open={modal.state.active}>
-        <div>
-          This is a modal that can do anything from feature announcements to
-          upsells.
-        </div>
-        <Button color="pink" onClick={() => transition('default')}>
-          Got it
-        </Button>
-      </Modal>
     </div>
   );
 }
