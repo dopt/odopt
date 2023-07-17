29fe83f0039878938bf1a02d36a1426b98b501d1
Alex Chan <alexchantastic@users.noreply.github.com>
2023-07-17 08:49:55 -0700

    feat(dopt): update checklist examples (#3283)

---
+++ apps/@examples/checklist-custom/src/components/checklist/list.tsx
@@ -0,0 +1,31 @@
+import { type ReactNode } from 'react';
+import { ChecklistProgress } from './progress';
+import { RichText } from '@dopt/react-rich-text';
+
+import './styles.css';
+
+export interface ChecklistProps {
+  title?: string | null;
+  body?: any;
+  value: number;
+  max: number;
+  children?: ReactNode;
+}
+
+export function Checklist(props: ChecklistProps) {
+  const { title, body, value, max, children } = props;
+  return (
+    <section className="checklist">
+      <header className="checklist__header">
+        <div className="checklist__content">
+          <h1 className="checklist__title">{title}</h1>
+          <div className="checklist__body">
+            <RichText>{body}</RichText>
+          </div>
+        </div>
+        <ChecklistProgress value={value} max={max} />
+      </header>
+      <ul className="checklist__items">{children}</ul>
+    </section>
+  );
+}
