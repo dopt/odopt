552af079037f31d25a0e607c09d106301924193a
Alex Chan <alexchantastic@users.noreply.github.com>
2023-07-18 10:40:17 -0700

    fix(@examples/tour-custom): use correct base path (#3313)

--- apps/@examples/tour-custom/vite.config.ts
+++ apps/@examples/tour-custom/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
       '@': '/src',
     },
   },
-  base: process.env.NODE_ENV === 'production' ? `/tour/` : '/',
+  base: process.env.NODE_ENV === 'production' ? `/tour-custom/` : '/',
   plugins: [react()],
 });
