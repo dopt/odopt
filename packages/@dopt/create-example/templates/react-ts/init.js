59cbf503dec32193568759765ce79b39f50e9b67
Pankaj Avhad <102550494+pankaj-avhad@users.noreply.github.com>
2023-08-15 10:49:30 -0700

    feat(@app/client): create a component flow template for create-example and support for add button (#3410)
    * feat(@app/client): create a component flow template and support for add button
    
    * fix(@app/client): fix typing and loading
    
    * fix(@app/client): add migration to delete the SbUserOnboardingJourney records
    
    * feat(@app/client): clean up create example flow logic
    
    * feat(@app/client): add disabled copy action prop
    
    * fix(@app/client): fix error state of button
    
    * chore(@app/server): revert db playground
    
    * fix(@app/server): remove data migration
    
    * fix(@app/client): update example flow template
    
    * feat(@dopt/create-example): update react and react-ts templates
    
    ---------
    
    Co-authored-by: Pankaj <pankaj@switchboard.cc>
    Co-authored-by: mariaorias <maria@switchboard.cc>
    Co-authored-by: Alex Chan <alex@dopt.com>

--- packages/@dopt/create-example/templates/react-ts/init.js
+++ packages/@dopt/create-example/templates/react-ts/init.js
@@ -4,11 +4,11 @@ import path from 'node:path';
 import { readFileSync, writeFileSync, rmSync } from 'node:fs';
 
 const args = process.argv.slice(2);
+const [BLOCKS_API_KEY, USER_ID, FLOW_ID] = args;
 
-async function init() {
+async function updateMainTsx() {
   const filePath = path.resolve('./src/main.tsx');
   const file = readFileSync(filePath, { encoding: 'utf8' });
-  const [BLOCKS_API_KEY, USER_ID, FLOW_ID] = args;
 
   let newFile = file;
 
@@ -20,11 +20,25 @@ async function init() {
     newFile = newFile.replace(/\$USER_ID/g, USER_ID);
   }
 
+  writeFileSync(filePath, newFile, { encoding: 'utf8' });
+}
+
+async function updateConstTs() {
+  const filePath = path.resolve('./src/const.ts');
+  const file = readFileSync(filePath, { encoding: 'utf8' });
+
+  let newFile = file;
+
   if (FLOW_ID) {
     newFile = newFile.replace(/\$FLOW_ID/g, FLOW_ID);
   }
 
   writeFileSync(filePath, newFile, { encoding: 'utf8' });
+}
+
+async function init() {
+  updateMainTsx();
+  updateConstTs();
 
   rmSync('./init.js');
 }
---
+++ packages/@dopt/create-example/templates/react/src/components/tour/popover.jsx
@@ -0,0 +1,31 @@
+import TourItem, { useTourItem } from '@dopt/react-tour';
+
+export function Popover(props) {
+  const { id, children } = props;
+  const item = useTourItem(id);
+
+  return (
+    <TourItem.Root active={item.active}>
+      <TourItem.Anchor>{children}</TourItem.Anchor>
+      <TourItem.Popover position="top">
+        <TourItem.Content>
+          <TourItem.Header>
+            <TourItem.Title>{item.title}</TourItem.Title>
+          </TourItem.Header>
+          <TourItem.Body>{item.body}</TourItem.Body>
+          <TourItem.Footer>
+            {item.index !== 0 && (
+              <TourItem.BackButton onClick={item.back}>
+                {item.backLabel}
+              </TourItem.BackButton>
+            )}
+            <TourItem.NextButton onClick={item.next}>
+              {item.nextLabel}
+            </TourItem.NextButton>
+          </TourItem.Footer>
+          <TourItem.Progress count={item.tour.size} index={item.index || 0} />
+        </TourItem.Content>
+      </TourItem.Popover>
+    </TourItem.Root>
+  );
+}
