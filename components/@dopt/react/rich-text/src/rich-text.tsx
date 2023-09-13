c29d5efc0c714fa54f5332b834bf41ea92465fff
Alex Chan <alexchantastic@users.noreply.github.com>
2023-09-13 13:55:41 -0700

    refactor(dopt): add explicit react import to published react packages (#3684)

--- components/@dopt/react/rich-text/src/rich-text.tsx
+++ components/@dopt/react/rich-text/src/rich-text.tsx
@@ -1,4 +1,4 @@
-import {
+import React, {
   forwardRef,
   type ForwardedRef,
   type ComponentPropsWithoutRef,
--- packages/@dopt/react/src/with-block.tsx
+++ packages/@dopt/react/src/with-block.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useBlock } from './use-block';
 
 /**
