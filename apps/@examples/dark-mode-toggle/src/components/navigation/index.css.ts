5a4681c344a450aed97f7169c9764abf8fc108ea
Doptimus Prime <github@dopt.com>
2023-11-28 21:25:45 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/dark-mode-toggle/src/components/navigation/index.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+export const navigation = style({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  fontWeight: 500,
+  fontSize: '16px',
+  gap: '32px',
+});
