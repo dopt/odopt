5962419d68db3e5156a7ab66c27515f538d413d2
Doptimus Prime <github@dopt.com>
2023-12-12 00:01:42 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/radix-dark-mode-toggle/src/pages/index.tsx
@@ -0,0 +1,74 @@
+import { example, heading, leftNav, rightNav } from '@/pages/index.css';
+
+import {
+  Alerts,
+  Brand,
+  Navigation,
+  Settings,
+  Table,
+  User,
+  Space,
+} from '@/components';
+
+import { useTheme } from '@/hooks';
+import { useModal } from '@dopt/react-modal';
+import RichTextField from '@dopt/react-rich-text';
+
+import * as Dialog from '@radix-ui/react-dialog';
+import { IconToggleRight } from '@tabler/icons';
+
+export function Example() {
+  const [_, setTheme] = useTheme('light');
+
+  const modal = useModal('radix-dark-mode-toggle.dark-mode-announcement');
+
+  return (
+    <div className={example}>
+      <div className={heading}>
+        <div className={leftNav}>
+          <Brand />
+          <Navigation />
+        </div>
+        <div className={rightNav}>
+          <Alerts />
+          <Settings />
+          <User />
+        </div>
+      </div>
+      <Table />
+      <Dialog.Root open={modal.active}>
+        <Dialog.Portal>
+          <Dialog.Overlay className="DialogOverlay" />
+          <Dialog.Content className="DialogContent">
+            <Space />
+            <div>
+              <Dialog.Title className="DialogTitle">{modal.title}</Dialog.Title>
+              <Dialog.Description className="DialogDescription">
+                <RichTextField>{modal.body}</RichTextField>
+              </Dialog.Description>
+              <div className="DialogButtons">
+                <Dialog.Close asChild>
+                  <button className="Button" onClick={() => modal.dismiss()}>
+                    {modal.dismissLabel}
+                  </button>
+                </Dialog.Close>
+                <Dialog.Close asChild>
+                  <button
+                    className="Button"
+                    onClick={() => {
+                      setTheme('dark');
+                      modal.complete();
+                    }}
+                  >
+                    <IconToggleRight />
+                    {modal.completeLabel}
+                  </button>
+                </Dialog.Close>
+              </div>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
+  );
+}
---
+++ apps/@examples/radix-dark-mode-toggle/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base:
+    process.env.NODE_ENV === 'production' ? `/radix-dark-mode-toggle/` : '/',
+  plugins: [react(), vanillaExtractPlugin()],
+});
