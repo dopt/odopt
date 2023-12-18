e3ffc503a1dcdff0e1c200f23148636c8ce2bc71
Doptimus Prime <github@dopt.com>
2023-12-18 19:08:44 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/invite-users/src/components/invite-teammates-form/index.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const textarea = style({
+  display: 'flex',
+  marginTop: '12px',
+  padding: '12px',
+  flexGrow: 1,
+  minHeight: '150px',
+  borderRadius: '4px',
+  border: '1px solid #CED4DA',
+  marginBottom: '24px',
+});
