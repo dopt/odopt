7cf707b791efe411e68dbd856b960f1ce6a7b42b
Doptimus Prime <github@dopt.com>
2023-12-22 20:53:58 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/radix-dark-mode-toggle/src/pages/_app.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import { DoptProvider } from '@dopt/react';
+import { useIdentifyUser } from '@dopt/react-users';
+import { nanoid } from 'nanoid';
+
+import { Example } from '@/pages/';
+
+import './_app.css';
+
+export function App() {
+  /**
+   * Create a static example user.
+   */
+  const user = useMemo(
+    () => ({
+      identifier: nanoid(),
+      properties: {
+        company: 'Dopt',
+        role: 'admin',
+        inTrial: true,
+      },
+    }),
+    []
+  );
+
+  /**
+   * Identify the example user to Dopt the first time the App loads.
+   */
+  const userId = useIdentifyUser(user);
+
+  return (
+    <DoptProvider
+      apiKey={import.meta.env.VITE_DOPT_BLOCKS_API_KEY}
+      userId={userId}
+      flowVersions={{ 'radix-dark-mode-toggle': 0 }}
+    >
+      <Example />
+    </DoptProvider>
+  );
+}
