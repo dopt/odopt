e957a66a437df8a62e464a630ecf41a70d9dea8e
Doptimus Prime <github@dopt.com>
2024-01-04 22:16:58 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/onboarding-hub/src/pages/_app.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from 'react';
+import { DoptProvider } from '@dopt/react';
+import { useIdentifyUser } from '@dopt/react-users';
+import { nanoid } from 'nanoid';
+
+import { Example } from '@/pages/';
+
+import { useChecklist } from '@dopt/react-checklist';
+
+import './_app.css';
+
+export function App() {
+  /**
+   * Create a static example user.
+   */
+  const user = useMemo(
+    () => ({
+      identifier: nanoid(),
+      properties: {
+        company: 'Dopt',
+        role: 'admin',
+        inTrial: true,
+      },
+    }),
+    []
+  );
+
+  /**
+   * Identify the example user to Dopt the first time the App loads.
+   */
+  const userId = useIdentifyUser(user);
+
+  return (
+    <DoptProvider
+      apiKey={import.meta.env.VITE_DOPT_BLOCKS_API_KEY}
+      userId={userId}
+      flowVersions={{
+        'onboarding-hub': 0,
+        'onboarding-hub-tour': 0,
+        'onboarding-hub-video': 0,
+      }}
+    >
+      <Example />
+      <DemoHelper />
+    </DoptProvider>
+  );
+}
+
+function DemoHelper() {
+  const checklist = useChecklist('onboarding-hub.getting-started-checklist');
+  checklist.items.forEach((item) => {
+    if (item.id !== 'onboarding-hub.invite-teammates' && !item.done) {
+      item.complete();
+    }
+  });
+
+  return <></>;
+}
---
+++ apps/@examples/onboarding-hub/src/pages/index.css.ts
@@ -0,0 +1,77 @@
+import { style, globalStyle } from '@vanilla-extract/css';
+
+globalStyle('body', {
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif',
+});
+
+export const example = style({
+  display: 'flex',
+  flexDirection: 'row',
+  flexGrow: 1,
+});
+
+export const heading = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'left',
+  justifyContent: 'space-between',
+  gap: '32px',
+  padding: '32px',
+  borderRight: '1px solid var(--color-border)',
+});
+
+export const leftNav = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '32px',
+});
+
+export const rightNav = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '16px',
+});
+
+export const user = style({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '8px',
+  alignItems: 'center',
+});
+
+export const onboardingHub = style({
+  display: 'flex',
+  flexDirection: 'row',
+  padding: '24px',
+  flex: 1,
+});
+
+export const checklist = style({});
+
+export const onboardingHubRightPane = style({
+  display: 'flex',
+  gap: '12px',
+  flexDirection: 'column',
+  padding: '0 24px',
+  flexGrow: 1,
+});
+
+export const resourceList = style({
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: '10px',
+  gap: '10px',
+});
+
+export const resourceLink = style({
+  display: 'flex',
+  flexDirection: 'row',
+  textDecoration: 'none',
+  gap: '5px',
+});
+
+export const demoVideo = style({});
+
+export const resources = style({});
