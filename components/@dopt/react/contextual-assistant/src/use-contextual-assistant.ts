8fc27f8bafea2aa4c97a2021847567a7f11acd69
Doptimus Prime <github@dopt.com>
2024-02-16 14:48:44 +0000

    chore(odopt): sync open-source changes to dopt/odopt

--- components/@dopt/react/contextual-assistant/src/use-contextual-assistant.ts
+++ components/@dopt/react/contextual-assistant/src/use-contextual-assistant.ts
@@ -1,112 +1,26 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { ContextualAssistantContext } from './contextual-assistant';
 
-import {
-  AnswerChunk,
-  ChatStreamChunk,
-  ContentStreamChunk,
-  DocumentsChunk,
-  StatusChunk,
-} from '@dopt/ai-assistant-definition';
-
-import { DoptAiContext } from '@dopt/ai-assistant-react';
-
-export function useContextualAssistant(sid: string) {
-  const { assistant } = useContext(DoptAiContext);
-  const { deregisterAssistant, registerAssistant, ...rest } = useContext(
-    ContextualAssistantContext
-  );
-
-  const [_, setChunks] = useState<ChatStreamChunk[]>([]);
-
-  const [status, setStatus] = useState<StatusChunk['status'] | null>(null);
-  const [documents, setDocuments] = useState<DocumentsChunk['sources'] | null>(
-    null
-  );
-  const [answer, setAnswer] = useState<AnswerChunk['answer'] | null>(null);
-  const [content, setContent] = useState<ContentStreamChunk['content'] | null>(
-    null
-  );
-
-  const clearState = useCallback(() => {
-    setDocuments(null);
-    setContent(null);
-    setStatus(null);
-    setAnswer(null);
-  }, [setDocuments, setAnswer, setContent, setStatus]);
-
-  useEffect(() => {
-    if (!rest.selection) {
-      clearState();
-    }
-  }, [rest.selection, clearState]);
-
-  useEffect(() => {
-    async function onSelectionCallback(element: HTMLElement) {
-      clearState();
-      const stream = await assistant.completions(sid, {
-        query: '',
-        context: {
-          element,
-        },
-      });
-
-      //@ts-ignore
-      const reader = (stream.stream as ReadableStream<Uint8Array>).getReader();
-      const decoder = new TextDecoder();
-
-      let done = false;
-      do {
-        const next = await reader.read();
-
-        if (next.value) {
-          const messages = decoder
-            .decode(next.value)
-            .split('\n')
-            .filter((m) => m);
-
-          for (const message of messages) {
-            const chunk = JSON.parse(message) as ChatStreamChunk;
-            setChunks((chunks) => {
-              return [...chunks, chunk];
-            });
-            switch (chunk.type) {
-              case 'status':
-                setStatus(chunk.status);
-                break;
-              case 'documents':
-                setDocuments(chunk.sources);
-                break;
-              case 'answer':
-                setAnswer(chunk.answer);
-                break;
-              case 'content':
-                setContent((content) =>
-                  content == null ? chunk.content : content + chunk.content
-                );
-                break;
-            }
-          }
-        }
-        done = next.done;
-      } while (!done);
-    }
-
-    registerAssistant(sid, onSelectionCallback);
-
-    return () => deregisterAssistant(sid, onSelectionCallback);
-  }, [assistant, registerAssistant, deregisterAssistant, sid, clearState]);
-
-  return {
+export function useContextualAssistant() {
+  const {
+    active,
     answer,
+    close,
     content,
     documents,
+    selection,
+    setActive,
     status,
-    closeAssistant: () => {
-      rest.setActive(false);
-      rest.setActive(false);
-      rest.setSelection(null);
-    },
-    ...rest,
+  } = useContext(ContextualAssistantContext);
+
+  return {
+    active,
+    answer,
+    close,
+    content,
+    documents,
+    selection,
+    setActive,
+    status,
   };
 }
