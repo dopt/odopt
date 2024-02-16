47a2a5edf4059d5a5dd785a0a45ae94f0c79914d
Doptimus Prime <github@dopt.com>
2024-02-16 14:53:06 +0000

    chore(odopt): sync open-source changes to dopt/odopt

--- packages/@dopt/ai-assistant/context/src/types.ts
+++ packages/@dopt/ai-assistant/context/src/types.ts
@@ -1,5 +1,5 @@
 import { Context } from '@dopt/ai-assistant-definition';
 
-export interface ContextGenerator<T extends Context> {
+export interface ContextGenerator<T extends Context | null> {
   generate: (...args: any) => Promise<T>;
 }
