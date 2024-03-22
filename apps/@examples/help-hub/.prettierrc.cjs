3deb6d18c3604ed7de609535c0c40f68261fed15
Doptimus Prime <github@dopt.com>
2024-03-22 23:03:00 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/help-hub/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+  useTabs: false,
+  endOfLine: 'lf',
+};
--- packages/@dopt/ai-assistant/react/src/index.ts
+++ packages/@dopt/ai-assistant/react/src/index.ts
@@ -2,6 +2,7 @@ export * from './context';
 export * from './provider';
 export * from './types';
 export * from './use-assistant';
+export * from './use-search';
 
 export * from '@dopt/ai-assistant-definition';
 export * from '@dopt/ai-assistant-context';
