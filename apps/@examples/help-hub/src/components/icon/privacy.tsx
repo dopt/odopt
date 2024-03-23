e65202cf89e30c3b5b63e28a152bad3122b746bb
Doptimus Prime <github@dopt.com>
2024-03-23 00:10:40 +0000

    chore(odopt): sync open-source changes to dopt/odopt

---
+++ apps/@examples/help-hub/src/components/icon/privacy.tsx
@@ -0,0 +1,18 @@
+export function Privacy() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 18 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 20C15 16.19 18 10.952 18 4.286C16 4.286 13 2.857 9 0C5 2.857 2 4.286 0 4.286C0 10.952 3 16.19 9 20ZM9 2.652C11.577 4.386 13.776 5.532 15.667 6.071C15.227 10.698 13.031 14.424 9 17.368V2.652Z"
+        fill="#8F95B2"
+      />
+    </svg>
+  );
+}
